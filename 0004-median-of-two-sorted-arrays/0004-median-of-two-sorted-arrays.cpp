895d2e130ede7b9d88e563f9fdf1d1b3bf859747
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-06-14 17:08:55 +0530

    Time: 78 ms (5.32%), Space: 89.7 MB (35.13%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,61 @@
 class Solution {
 public:
-    int p1 = 0, p2 = 0;
-
-    // Get the smaller value between nums1[p1] and nums2[p2] and move the pointer forward.
-
-    int getMin(vector<int>& nums1, vector<int>& nums2) {
-        if (p1 < nums1.size() && p2 < nums2.size()) {
-            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
-        } else if (p1 < nums1.size()) {
-            return nums1[p1++];
-        } else if (p2 < nums2.size()) {
-            return nums2[p2++];
-        } 
-        return -1;
-    }
     
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = int(nums1.size()), n = int(nums2.size());
+    double min(vector<int>& nums1, vector<int>& nums2){
+        int m = nums1.size();
+        int n =nums2.size();
+        int k =m+n;
+        vector<int> ans(k);
+        int p=0;
+        int q=0;
         
-        if ((m + n) % 2 == 0) {
-            for (int i = 0; i < (m + n) / 2 - 1; ++i) {
-                int _ = getMin(nums1, nums2);
+        for(int i=0;i<k;i++){
+        if(p<m  && q<n){
+            if(nums1[p]<=nums2[q]){
+                ans[i]=nums1[p];
+                p++; 
             }
-            return (double)(getMin(nums1, nums2) + getMin(nums1, nums2)) / 2;
-        } else {
-            for (int i = 0; i < (m + n) / 2; ++i) {
-                int _ = getMin(nums1, nums2);
+            else if(nums1[p]>nums2[q]){
+                ans[i]=nums2[q];
+                q++; 
             }
-            return getMin(nums1, nums2);
+        }
+        else if(p<m){
+            ans[i]=nums1[p];
+            p++;
+        }
+        else if(q<n){
+            ans[i]=nums2[q];
+            q++;
         }
             
-        return -1;
+        
+    }
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i];
+        }
+        double median = 0.00000;
+        if(k==2){
+            median=(ans[1]+ans[0])/2.0;
+        }
+        else if(k%2==0 && k!=2){
+            median = (ans[(k/2)-1] + ans[(k/2)]) / 2.0;
+
+            }
+        else if(k%2!=0 && k!=2) {
+            median = ans[(k-1)/2.0];
+        }
+        return median;
+    }
+    
+    
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+       
+        double median=min(nums1, nums2);
+        return median;
+        
+        
+        
     }
 };
