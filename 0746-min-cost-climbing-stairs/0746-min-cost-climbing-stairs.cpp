b8b4da1e2be9922a6080107a216658fbb690a049
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-06-15 00:27:12 +0530

    Time: 7 ms (65.20%), Space: 13.7 MB (74.24%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -18,11 +18,23 @@ public:
         
         
     }
+    int mincost2(vector<int>& cost, int n){
+        vector<int>dp(n+1);
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+        }
+        return min(dp[n-1],dp[n-2]);
+        
+        
+    }
     
     int minCostClimbingStairs(vector<int>& cost) {
         int n= cost.size();
-        vector<int>dp (n+1,-1);
-        int ans=min(mincost(cost,n-1,dp),mincost(cost,n-2,dp));
-        return ans;
+        // vector<int>dp (n+1,-1);
+        // int ans=min(mincost2(cost,n-1,dp),mincost2(cost,n-2,dp));
+        // return ans;
+        return mincost2(cost,n);
     }
 };
