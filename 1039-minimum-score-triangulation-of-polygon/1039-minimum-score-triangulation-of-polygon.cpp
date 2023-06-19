99766d62581c2a3b92538c95e9f0a42c8f83d855
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-06-19 14:21:53 +0530

    Time: 10 ms (54.20%), Space: 8.5 MB (72.70%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -24,24 +24,48 @@ public:
 //         }
     // }
     
-    int solve(vector<int> &val, int i, int j,vector<vector<int>>& dp){
-        int n = val.size();
-        if(i+1==j){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+    // int solve(vector<int> &val, int i, int j,vector<vector<int>>& dp){
+    //     int n = val.size();
+    //     if(i+1==j){
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     int ans = INT_MAX;
+    //     for(int k=i+1; k<j; k++){
+    //         ans = min(ans, val[i]*val[j]*val[k]+solve(val,i,k,dp)+solve(val,k,j,dp));
+    //         dp[i][j]=ans;
+    //     }
+    //     return dp[i][j];
+    // }
+     int solve(vector<int> &val){
+         int n = val.size();
+         vector<vector<int>> dp(n, vector<int>(n,0));
+        // int n = val.size();
+        // if(i+1==j){
+        //     dp[i][j]=0;
+        // }
+        // if(dp[i][j]!=-1){
+        //     return dp[i][j];
+        // }
+        for(int i=n-1;i>=0;i--){
+            for(int j= i+2;j<n;j++){// i+2 to ensure atleast 3 vertices
+        
         int ans = INT_MAX;
         for(int k=i+1; k<j; k++){
-            ans = min(ans, val[i]*val[j]*val[k]+solve(val,i,k,dp)+solve(val,k,j,dp));
-            dp[i][j]=ans;
+            ans = min(ans, val[i]*val[j]*val[k]+dp[i][k]+dp[k][j]);
+           
         }
-        return dp[i][j];
+                 dp[i][j]=ans;
+                      }}
+        // return dp[i][j];
+                      return dp[0][n-1];
     }
     int minScoreTriangulation(vector<int>& values) {
         int n = values.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return solve(values,0,n-1,dp);
+        // vector<vector<int>> dp(n,vector<int>(n,-1));
+        // return solve(values,0,n-1,dp);
+        return solve(values);
     }
 };
