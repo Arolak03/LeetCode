ef4db0906a437496fcfb1caedee7d90104f4babb
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-06-20 21:35:05 +0530

    Time: 43 ms (41.47%), Space: 20.8 MB (31.77%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -16,29 +16,44 @@ public:
     //     return array;
     // }
     
-    int sat(vector<int>& arr, int index, int time,vector<vector<int>>& dp){
-        int n = arr.size();
-        if(index==n){
-            return 0;
-        }
-        if(dp[index][time]!=-1)
-           {
-               return dp[index][time];
-           }        
-        int incl = arr[index]*(time+1) + sat(arr, index+1, time+1,dp);
-        int excl = 0 + sat(arr,index+1,time,dp);
+//     int sat(vector<int>& arr, int index, int time,vector<vector<int>>& dp){
+//         int n = arr.size();
+//         if(index==n){
+//             return 0;
+//         }
+//         if(dp[index][time]!=-1)
+//            {
+//                return dp[index][time];
+//            }        
+        // int incl = arr[index]*(time+1) + sat(arr, index+1, time+1,dp);
+        // int excl = 0 + sat(arr,index+1,time,dp);
         
-        dp[index][time]=max(incl,excl);
-        return dp[index][time];
+//         dp[index][time]=max(incl,excl);
+//         return dp[index][time];
+//  }
+    int sat(vector<int>& arr){
+        int n = arr.size();
+        vector<vector<int>>dp (n+1,vector<int>(n+1,0));
+        dp[n][n] = 0;
+        int ans = 0;
+        for(int index = n-1 ;index>=0;index--){
+            for(int time =index;time>=0;time--){
+                int incl = arr[index]*(time+1) + dp[index+1][time+1];
+                int excl = 0 + dp[index+1][time];
+                dp[index][time] = max(incl,excl);
+            }
+        }
+        // dp[index][time]=ans;
+        return dp[0][0];
  }
     
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
         int n = satisfaction.size();
         // return sort(satisfaction);
-        vector<vector<int>>dp (n+1,vector<int>(n+1,-1));
+        // vector<vector<int>>dp (n+1,vector<int>(n+1,-1));
         
-        return sat(satisfaction,0,0,dp);
+        return sat(satisfaction);
         
     }
 };
