a0712c23be5592cdefbbd3700daaacac52c28baf
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-06-25 10:27:05 +0530

    Time: 3 ms (49.18%), Space: 5.9 MB (72.75%) - LeetHub

---
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        int res = 0;
+        for(int i=0;i<17;i++){
+            if(__builtin_popcount(n+(1<<i))<__builtin_popcount(n)){
+                n = n+(1<<i);
+                ++res;
+            }
+        }
+        return res + __builtin_popcount(n);
+        
+    }
+};
