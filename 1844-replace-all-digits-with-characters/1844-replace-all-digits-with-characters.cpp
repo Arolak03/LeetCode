447ba78d602179747616e2fab529c94eebf183b6
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-01 23:39:46 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (79.63%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -3,6 +3,8 @@ public:
     string replaceDigits(string s) {
          for(int i=1;i<s.length();i+=2)
          s[i]+=s[i-1]-'0';
+        // string m =('1'+'a'-'0');
         return s;
+        
     }
 };
