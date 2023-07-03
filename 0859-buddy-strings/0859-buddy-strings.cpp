ecde008d2ae2fdcc72dde531b1d6b2221801bcde
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-03 23:44:03 +0530

    Time: 7 ms (35.87%), Space: 7 MB (52.51%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
-         int ns=s.size(),ng=goal.size(); // size same na ho
-        if(ns != ng ) {
-            return 0;
-        }
-        if(s == goal ) { // agr same h
-            vector<int>farr(26,0);
-            for(auto &x:s){
-                farr[x-'a']++;
-                if(farr[x-'a']==2)
-                return true;
+        if(s.size()!=goal.size() || s.size()==1)return false;
+        vector<int>different;
+        vector<int>ct(26,0);
+        bool f=false;
+        for(int i=0;i<s.size();i++){
+            
+            if(s[i]!=goal[i])different.push_back(i); //index rkha
+            
+            else{// if 2 same char in string s uski ascii value bhada rhi hu aur agr wo 2 y 2 s jyada h to swap krne pr bhi same hi aayega
+                ct[s[i]-'a']++;
+                if(ct[s[i]-'a']==2)f=true;
             }
-            return false;;
-        }
-        vector<int> ans;
-        for(int i=0;i<ns;i++)
-        {
-            if(s[i] != goal[i]) {
-                ans.push_back(i);
-            }
-            if(ans.size()>2)
-            return false;
         }
         
-        return  ans.size()==2&&s[ans[0]]==goal[ans[1]] && s[ans[1]]==goal[ans[0]];
+        if(different.size()>2)return false;
+        if(different.size()==1)return false;
+         if(different.size()==0){
+             return f;
+         }
+        int in1,in2;
+        in1 = different[0];
+        in2 = different[1];
+        swap(s[in1],s[in2]);
+        return s==goal;
     }
 };
