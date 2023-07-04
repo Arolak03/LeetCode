3a156c3731959ad4767bcd1953113a44df33cd0b
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-05 01:21:53 +0530

    Time: 15 ms (43.86%), Space: 20.7 MB (8.80%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -15,29 +15,49 @@
 
 class Solution {
     
-    private:
+//     private:
     
-    int height(TreeNode* root){
-    if(root==NULL){
-        return 0;
-    }
-    int left = height(root -> left);
-    int right = height(root -> right);
-    int ans = max(left, right)+1;
-    return ans;
-}
+//     int height(TreeNode* root){
+//     if(root==NULL){
+//         return 0;
+//     }
+//     int left = height(root -> left);
+//     int right = height(root -> right);
+//     int ans = max(left, right)+1;
+//     return ans;
+// }
     
     public:
     
+    pair<int, int> solve(TreeNode* root){
+        if(root==NULL){
+            pair<int, int> p = make_pair(0,0);
+            return p;
+        }
+        pair<int, int> left = solve(root->left);
+        
+        pair<int, int> right = solve(root -> right);
+        int op1=left.first;
+        int op2=right.first;
+        int op3=left.second+right.second;
+        pair<int, int> ans;
+        
+        ans.second = max(left.second, right.second)+1;
+        ans.first = max(op1, max(op2,op3) );
+        return ans;
+        
+    }
+    
     
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root==NULL){
-            return 0;
-        }
-        int left = diameterOfBinaryTree(root->left);
-        int right = diameterOfBinaryTree(root->right);
-        int both = height(root->left) + height(root -> right) ;
-        int ans = max(left, max(right, both));
-        return ans;
+        // if(root==NULL){
+        //     return 0;
+        // }
+        // int left = diameterOfBinaryTree(root->left);
+        // int right = diameterOfBinaryTree(root->right);
+        // int both = height(root->left) + height(root -> right) ;
+        // int ans = max(left, max(right, both));
+        // return ans;
+        return solve(root).first;
     }
 };
