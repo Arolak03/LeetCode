da2f5b65df9c1b303eeaf21f794e3c3f3027e4e0
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-05 12:03:31 +0530

    Time: 17 ms (42.35%), Space: 22.1 MB (6.62%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,31 +11,51 @@
  */
 class Solution {
     
-    private:
-    int height(TreeNode* root){
-        if(root==NULL){
-            return 0;
+   
+public:
+    
+    pair<int, int> solve(TreeNode* root){
+        if(root == NULL){
+            pair<bool, int>p = make_pair(true,0);
+            return p;
         }
-        int left = height(root->left);
-                int right=height(root->right);
-        int ans = max(left,right)+1;
+        pair<int, int> left = solve(root ->left);
+        pair<int, int> right = solve(root ->right);
+        
+        //first rue false
+        //second height
+        bool leftt = left.first;
+        bool rightt=right.first;
+         bool diff = abs(left.second - right.second) <=1;
+        pair<bool, int> ans;
+        ans.second = max(left.second , right.second)+1;
+       
+        if(leftt && rightt && diff){
+            ans.first = true;
+        }
+        else{
+            ans.first = false;
+        }
         return ans;
     }
     
-public:
+    
     bool isBalanced(TreeNode* root) {
-        //left - right <= 1
-        if(root==NULL){
-            return true;
-        }
-        bool left = isBalanced(root -> left);
-        bool right = isBalanced(root -> right);
-        bool condn = abs(height(root->left)-height(root->right))<=1;
-        if(left && right && condn){
-            return true;
-        }
-        else{
-            return false;
-        }
+        // //left - right <= 1
+        // if(root==NULL){
+        //     return true;
+        // }
+        // bool left = isBalanced(root -> left);
+        // bool right = isBalanced(root -> right);
+        // bool condn = abs(height(root->left)-height(root->right))<=1;
+        // if(left && right && condn){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+        return solve(root).first;
+        
+        
     }
 };
