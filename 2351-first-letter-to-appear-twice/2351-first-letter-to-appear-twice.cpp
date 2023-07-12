da0a0a2f3335708a14847ac7e02d38db983d9cb2
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-12 22:42:21 +0530

    Time: 4 ms (30.09%), Space: 6.1 MB (85.06%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     char repeatedCharacter(string s) {
-        char c = '\0'; // Initialize c with a default value
-        vector<int> letter(26, 0);
-        for (int i = 0; i < s.length(); i++) {
-            letter[s[i] - 'a']++;
-            if (letter[s[i] - 'a'] == 2) {
-                c = s[i];
-                break; // Break the loop since we found the repeated character
+        char c='\0';
+        vector<int> letter(26,0);
+        for(int i=0;i<s.length();i++){
+            letter[s[i]-'a']++  ;
+            if(letter[s[i]-'a']==2){
+                c=s[i];
+                break;
             }
         }
         return c;
