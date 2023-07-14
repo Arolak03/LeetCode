b9461d33c980c02059b1e89d2a80a98b978efaa5
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-15 00:11:35 +0530

    Time: 148 ms (89.39%), Space: 56.6 MB (30.31%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-    int longestSubsequence(vector<int>& arr, int difference) {
-        int n = arr.size();
-        unordered_map<int, int> dp;
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            int temp = arr[i] - difference;
-            int tempAns=0;
-            
-            // agr left m temp hai to uska ans mera tempAns ho jayega
-            if(dp.count(temp)){
-                tempAns = dp[temp];
-            }
-            //aur arr[i] m wo tempAns +1 ho jayega
-            dp[arr[i]] = 1+tempAns;
-            ans = max(ans,dp[arr[i]]);
+    int longestSubsequence(std::vector<int>& arr, int d) {
+        std::unordered_map<int, int> mp;
+        int mx = 0;
+        for (int i : arr) {
+            mp[i] = mp[i - d] + 1;
+            mx = std::max(mp[i], mx);
         }
-        return ans;
-        
+        return mx;
     }
 };
