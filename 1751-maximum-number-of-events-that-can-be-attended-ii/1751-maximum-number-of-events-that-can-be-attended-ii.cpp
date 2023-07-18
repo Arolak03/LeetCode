5cee8f810a286c13209d33aee37a03aab5b3fb12
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-18 16:11:30 +0530

    Time: 299 ms (78.31%), Space: 75.8 MB (27.55%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int solve(const vector<vector<int>>& events, int k, int index, vector<vector<int>>& dp) {
+        if (index >= events.size() || k == 0) {
+            return 0;
+        }
+
+        if (dp[index][k] != -1) {
+            return dp[index][k];
+        }
+
+        // Include the current event
+        int sum = events[index][2];
+        int nextEventIndex = index + 1;
+        while (nextEventIndex < events.size() && events[nextEventIndex][0] <= events[index][1]) {
+            nextEventIndex++;
+        }
+        int maxsum = sum + solve(events, k - 1, nextEventIndex, dp);
+
+        // Exclude the current event
+        int maxsumWithoutCurrent = solve(events, k, index + 1, dp);
+
+        dp[index][k] = max(maxsum, maxsumWithoutCurrent);
+        return dp[index][k];
+    }
+
+    int maxValue(vector<vector<int>>& events, int k) {
+        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+
+        // Initialize dp with -1
+        vector<vector<int>> dp(events.size(), vector<int>(k + 1, -1));
+
+        return solve(events, k, 0, dp);
+    }
+};
