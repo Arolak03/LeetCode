e88abf90515633965f3828ca7d23c7a80906b45b
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-20 16:19:32 +0530

    Time: 16 ms (78.66%), Space: 11.4 MB (86.62%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -8,63 +8,41 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
-private:
-    pair<ListNode*, ListNode*> reverseSubPart(ListNode* start, ListNode* end) {
-        ListNode* prev = nullptr;
-        ListNode* cur = start;
-        ListNode* future = cur->next;
-        while (cur->next && cur != end) {
-            cur->next = prev;
-            prev = cur;
-            cur = future;
-            future = cur->next;
-        }
-        cur->next = prev;
-        ListNode* ptr = cur;
-        while (ptr->next) {
-            ptr = ptr->next;
-        }
-        return {cur, ptr};
-    }
-
 public:
+    
+    
+    void reverse(ListNode* start, ListNode* end){
+        ListNode* prev  = NULL;
+        ListNode* curr = start;
+        ListNode* n = start->next;
+        while(prev != end){
+            curr -> next = prev;
+            prev = curr;
+            curr = n;
+             if(n!=NULL){
+                n=n->next;
+            }
+        }
+       
+    }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (head == nullptr || head->next == nullptr || k == 1) {
+        
+        
+        if(head==NULL || head->next==NULL || k==1){
             return head;
         }
-
-        ListNode* start = head;
-        ListNode* end = head;
-        for (int i = 0; i < k - 1; ++i) {
+        ListNode* start=head;
+        ListNode* end=head;
+        int increase = k-1;
+        while(increase--){
             end = end->next;
+            if(end==NULL)return head;
         }
-        ListNode* res = end;
-        ListNode* prev = nullptr;
-
-        start = head;
-        end = head;
-        while (start) {
-            for (int i = 0; i < k - 1; ++i) {
-                if (end == nullptr) {
-                    break;
-                }
-                end = end->next;
-            }
-            if (end == nullptr) {
-                break;
-            }
-
-            ListNode* temp = end->next;
-            auto [newhead, newend] = reverseSubPart(start, end);
-            newend->next = temp;
-            if (prev != nullptr) {
-                prev->next = newhead;
-            }
-            prev = newend;
-            start = end = newend->next;
-        }
-        return res;
+        ListNode* newHead = reverseKGroup(end->next,k);
+        reverse(start,end);
+        start->next=newHead;
+        return end;
+            
     }
 };
