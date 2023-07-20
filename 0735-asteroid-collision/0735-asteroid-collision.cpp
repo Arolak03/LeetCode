48bf4d50d39a126dd6c1d87d684683fbd5fd6389
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-20 13:20:23 +0530

    Time: 13 ms (77.16%), Space: 17.7 MB (55.66%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> asteroidstack;
-        for(int asteroid : asteroids){
-        while (!asteroidstack.empty() && asteroid<0 && asteroidstack.top()>0){
-            int diff = asteroid + asteroidstack.top();
-            if(diff<0){
-                asteroidstack.pop();
+        stack<int> asteroidStack;
+        
+        for(int asteroid: asteroids){
+            while(!asteroidStack.empty() && asteroid<0 && asteroidStack.top()>0){
+                int diff = asteroid + asteroidStack.top();
+                // if positive then asteroid bada
+                if(diff <0){
+                    asteroidStack.pop();
+                    // break;
+                    // asteroidStack.push(asteroid);
+                }
+                else if(diff >0){
+                    asteroid =0;
+                    break;
+                }
+                else{
+                    asteroid=0;
+                    asteroidStack.pop();
+                }
             }
-            else if(diff>0){
-                asteroid=0;
-                break;
-            }
-            else{
-                asteroid=0;
-                asteroidstack.pop();
+            if(asteroid!=0){
+                asteroidStack.push(asteroid);
             }
         }
-        if(asteroid!=0){
-            asteroidstack.push(asteroid);
-        }
-        }
-        vector<int>arr(asteroidstack.size());
-        int i = asteroidstack.size()-1;
-        while(!asteroidstack.empty()){
-            arr[i--]= asteroidstack.top();
-            asteroidstack.pop();
-        }
+            // asteroid left daalege
+            vector<int> arr(asteroidStack.size());
+            int n = asteroidStack.size()-1;
+            while(!asteroidStack.empty()){
+                arr[n--]=asteroidStack.top();
+                asteroidStack.pop();
+            }
+            
+            
+        
         return arr;
     }
 };
