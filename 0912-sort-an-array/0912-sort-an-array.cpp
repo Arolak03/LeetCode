6e6063805399822300014f4fd9f06b1e3a4e3e08
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-22 15:41:37 +0530

    Time: 770 ms (31.06%), Space: 181.1 MB (25.92%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    
+    void merge(vector<int>& nums, int s, int e){
+               int mid = s+(e-s)/2;
+        //new array bnaao
+        int len1=mid-s+1;
+        int len2=e-mid;
+        
+        vector<int> f(len1);
+        vector<int> sec(len2);
+        int k1 =s;
+        for(int i=0;i<len1;i++){
+            f[i]=nums[k1++];
+        }
+        int k = mid+1;
+        for(int i=0;i<len2;i++){
+            sec[i]=nums[k++];
+        }
+        int index1=0;
+        int index2=0;
+        //both filled now i have to add
+        int k3 = s;
+        while(index1<len1 && index2<len2){
+            if(sec[index2]<f[index1]){
+                nums[k3++]=sec[index2++];
+            }
+            else{
+                nums[k3++]=f[index1++];
+            }
+        }
+        while(len1>index1){
+            nums[k3++]=f[index1++];
+        }
+        while(len2>index2){
+            nums[k3++]=sec[index2++];
+        }
+        
+    }
+    
+    void mergeSort(vector<int>& nums, int s, int e){
+        int mid = s+(e-s)/2;
+        //break
+        if(s>=e){
+            return;
+        }
+        mergeSort(nums,s,mid);
+        mergeSort(nums,mid+1,e);
+        
+        merge(nums,s,e);
+    }
+    
+    
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums,0,nums.size()-1);
+        return nums;
+    }
+};
