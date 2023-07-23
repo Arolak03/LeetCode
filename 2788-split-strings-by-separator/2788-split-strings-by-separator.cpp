b0605342a928ba54ca71deca1fab45efc199d133
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-23 21:28:07 +0530

    Time: 39 ms (42.86%), Space: 39 MB (14.29%) - LeetHub

---
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> splitWordsBySeparator(vector<string>& words, char separator) {
+        vector<string> ans;
+        for(int i=0;i<words.size();i++){
+            string s = words[i];
+            string temp = "";
+            for(auto letter: s){
+                if(letter == separator && temp!=""){
+                    ans.push_back(temp);
+                    temp="";
+                    
+                }
+                if(letter!=separator){
+                    temp+=letter;
+                }
+            
+            }
+            if(temp!=""){
+                ans.push_back(temp);
+            }
+        }
+        return ans;
+    }
+};
