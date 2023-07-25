74e50697b01484dea1f20920fe9db051a3c54d35
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-25 20:02:59 +0530

    Time: 13 ms (19.23%), Space: 8.7 MB (72.83%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -1,41 +1,42 @@
-
 class Solution {
 public:
- vector<ListNode*> splitListToParts(ListNode* head, int k) {
- vector<ListNode*> ans;
- int n = 0;
- ListNode* temp = head;
- while (temp != nullptr) {
- n++;
- temp = temp->next;
- }
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector<ListNode*> ans;
+        int n = 0;
+        ListNode* temp = head;
+        
+        // Count the number of nodes in the linked list
+        while (temp != nullptr) {
+            n++;
+            temp = temp->next;
+        }
 
-int parts = n / k;
- int rem = n % k;
- 
-temp = head;
- while (k > 0) {
- int partSize = parts;
- if (rem > 0) {
- partSize++;
- rem--;
- }
+        int parts = n / k;
+        int rem = n % k;
+        
+        temp = head;
+        while (k > 0) {
+            int partSize = parts;
+            if (rem > 0) {
+                partSize++;
+                rem--;
+            }
 
-if (partSize == 0) {
- ans.push_back(nullptr);
- } else {
- ans.push_back(temp);
- for (int i = 0; i < partSize - 1; i++) {
- temp = temp->next;
- }
- ListNode* nextTemp = temp->next;
- temp->next = nullptr;
- temp = nextTemp;
- }
- 
-k--;
- }
+            if (partSize == 0) {
+                ans.push_back(nullptr);
+            } else {
+                ans.push_back(temp);
+                for (int i = 0; i < partSize - 1; i++) {
+                    temp = temp->next;
+                }
+                ListNode* nextTemp = temp->next;
+                temp->next = nullptr;
+                temp = nextTemp;
+            }
+            
+            k--;
+        }
 
-return ans;
- }
+        return ans;
+    }
 };
