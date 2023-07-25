b1aa434819e7926dee50d98d03d666987cdce221
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-25 19:31:22 +0530

    Time: 142 ms (7.61%), Space: 59.6 MB (40.39%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& arr){
-        int s = 0;
-        int e = arr.size()-1;
-        int mid = s+(e-s)/2;
-        while(s<e){
-            if(arr[mid]<arr[mid+1]){
-                s = mid+1;
-            }
-            else{
-                e=mid;
-            }
-            mid = s+(e-s)/2;
-        }
-        
-        return mid;
-    }
-    
     int peakIndexInMountainArray(vector<int>& arr) {
-        // int n = arr.len()
-        int ans = binarySearch(arr);
-        return ans;
+        int i=0;
+        while(arr[i]<arr[i+1]){
+            i++;
+        }
+        return i;
     }
 };
