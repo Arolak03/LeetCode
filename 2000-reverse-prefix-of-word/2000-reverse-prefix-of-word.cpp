cb5a201bb3448bcb8634ac076b30d39740330bd2
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-27 00:21:47 +0530

    Time: 4 ms (41.24%), Space: 6.3 MB (24.64%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int index=-1;
+        for(int i=0;i<word.length();i++){
+            if(word[i]==ch){
+                index=i;
+                break;
+            }
+        }
+        if(index!=-1){
+            int start=0;
+            int end=index;
+            while(start<end){
+                swap(word[start],word[end]);
+                start++;
+                end--;
+            }
+        }
+        return word;
+    }
+};
