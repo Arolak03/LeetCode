8a10f1948ca0e926d59b95a38e6865e44c25bdb9
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-26 11:24:13 +0530

    Time: 248 ms (18.39%), Space: 30 MB (98.53%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         vector<string> ans;
-        int index1 = INT_MAX; // Initialize with a large value
-        for (int i = 0; i < list1.size(); i++) {
-            for (int j = 0; j < list2.size(); j++) {
-                if (list1[i] == list2[j]) {
-                    int indexSum = i + j;
-                    if (indexSum < index1) {
+        int index1=INT_MAX;
+        // int index2=0;
+        for(int i=0; i<list1.size();i++){
+            for(int j= 0;j<list2.size();j++){
+                if(list1[i]==list2[j]){
+                    int index = i+j;
+                    if(index<index1){
                         ans.clear();
                         ans.push_back(list1[i]);
-                        index1 = indexSum;
-                    } else if (indexSum == index1) {
+                        index1=index;
+                    }
+                    else if(index==index1){
                         ans.push_back(list1[i]);
                     }
                 }
             }
         }
-
-        // Sort the result lexicographically
-        sort(ans.begin(), ans.end());
+           
+        
+        
+        
         return ans;
     }
 };
