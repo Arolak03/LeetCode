37d93846f8552b97fb30a4873e83bffbde0a8799
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-27 23:42:37 +0530

    Time: 449 ms (100.00%), Space: 191.5 MB (60.50%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    long long ans = 0;
-
-    long long dfs(int node, vector<vector<int>>& adj, vector<bool>& vis, int seats) {
+    long long ans=0;
+    long long dfs(int node,vector<vector<int>>& adj, vector<bool>& vis,int seats){
         // node ko vis
-        vis[node] = true;
-        long long count = 1;
-        for (auto child : adj[node]) {
-            if (!vis[child]) {
-                count += dfs(child, adj, vis, seats);
+        vis[node]=true;
+        int count=1;
+        for(auto child: adj[node]){
+            if(!vis[child]){
+                count+=dfs(child,adj,vis,seats);
             }
         }
-        long long x = (count / seats);
-        if (count % seats) x++;
-        if (node != 0) {
-            ans += x;
+        long long x = (count/seats);
+        if(count%seats)x++;
+        if(node!=0){
+            ans+=x;
         }
         return count;
+        
     }
-
+    
+    
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
         // adj bnaao
-        if (roads.size() == 0) {
+        int n= roads.size()+1;
+        if(roads.size()==0){
             return 0;
         }
-        int n = roads.size()+1;
-        
-        // n++; // Increment n to get the number of nodes in the graph
         vector<vector<int>> adj(n);
-        vector<bool> vis(n, false);
-        for (int i = 0; i < roads.size(); i++) {
+        for(int i=0;i<roads.size();i++){
             int v = roads[i][0];
             int u = roads[i][1];
             adj[v].push_back(u);
             adj[u].push_back(v);
         }
-        dfs(0, adj, vis, seats);
+        vector<bool> vis(n,0);
+        dfs(0,adj,vis,seats);
         return ans;
     }
 };
