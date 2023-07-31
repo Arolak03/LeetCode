d30f7d54c62f7cb088e4d40d17398eee31167641
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-31 11:28:26 +0530

    Time: 508 ms (42.30%), Space: 292.2 MB (15.93%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,32 +4,28 @@
 
 class Solution {
 public:
-    int solve(vector<int>& nums, int prev, int index, vector<vector<int>>& dp) {
-        if (index >= nums.size()) {
-            return 0;
+        int bottomUp(vector<int>& nums){
+        vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,0));
+
+        for(int curr=nums.size()-1;curr>=0;curr--){
+            for(int prev=curr-1;prev>=-1;prev--){
+
+                 
+                    int include=0;
+                    if(prev==-1||nums[curr]>nums[prev])
+                        include=1+dp[curr+1][curr+1];
+                    
+                    //exclude
+                    int exclude=dp[curr+1][prev+1];
+                    dp[curr][prev+1]= max(include,exclude);
+                    
+                }
         }
-        if(dp[prev+1][index]!=-1){
-            return dp[prev+1][index];
-        }
-        
-        // If we include the current element, we need to find the LIS from the next index
-        int include = 0;
-        if (prev ==-1 || nums[index] > nums[prev]) {
-            include = 1 + solve(nums, index, index + 1,dp);
-        }
-        
-        // If we exclude the current element, we need to find the LIS from the next index
-        int exclude = solve(nums, prev, index + 1,dp);
-        
-        // Return the maximum LIS obtained from including or excluding the current element
-        return dp[prev+1][index]=max(include, exclude);
+        return dp[0][0];
     }
-    
     int lengthOfLIS(vector<int>& nums) {
-        // Start from the first element (index 0) and the previous index as -1
-        vector<vector<int>> dp(nums.size()+1, vector<int>(nums.size()+1,-1));
-        int prev=-1;
-        int index=0;
-        return solve(nums, prev, index,dp);
+         int prev=-1,curr=0;
+        return bottomUp(nums);
     }
 };
+
