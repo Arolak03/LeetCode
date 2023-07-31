901176d9b55d16385cb62f7dd81b9b70cf2166f0
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-07-31 11:02:19 +0530

    Time: 45 ms (69.69%), Space: 15.2 MB (68.25%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int m = s1.length();
+        int n = s2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // Fill up the first row and first column for base cases
+        for (int i = 1; i <= m; ++i) {
+            dp[i][0] = dp[i - 1][0] + static_cast<int>(s1[i - 1]);
+        }
+
+        for (int j = 1; j <= n; ++j) {
+            dp[0][j] = dp[0][j - 1] + static_cast<int>(s2[j - 1]);
+        }
+
+        // Fill up the DP table
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j] + static_cast<int>(s1[i - 1]),
+                                   min(dp[i][j - 1] + static_cast<int>(s2[j - 1]),
+                                       dp[i - 1][j - 1] + static_cast<int>(s1[i - 1] + s2[j - 1])));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
