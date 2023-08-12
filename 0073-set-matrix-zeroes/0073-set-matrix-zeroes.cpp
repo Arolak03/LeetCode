544b4ef80f88380196164b6b7475aa1aef7d65f3
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-08-12 11:31:53 +0530

    Time: 6 ms (98.48%), Space: 13.9 MB (6.65%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
+        int m= matrix.size();
         int n = matrix[0].size();
-        
-        vector<pair<int, int>> zeros; // To store the positions of zeros
-        
-        // Find the positions of zero elements
-        for (int i = 0; i < m*n; ++i) {
-            int row=i/n;
-            int col =i%n;
-        
-                if (matrix[row][col] == 0) {
-                    zeros.push_back(make_pair(row, col));
+        vector<vector<int>> mat=matrix;
+        for(int i=0;i<m*n;i++){
+            int row = i/n;
+            int col = i%n;
+            if(matrix[row][col]==0){
+                //row same rkhna h 
+                for(int j=0;j<m;j++){
+                    mat[j][col]=0;
                 }
-            
-        }
-        
-        // Set entire rows and columns to zero based on the stored positions
-        for (const auto& zero : zeros) {
-            int row = zero.first;
-            int col = zero.second;
-            
-            for (int i = 0; i < m; ++i) {
-                matrix[i][col] = 0; // Zero out the column
+                //col same
+                for(int j=0;j<n;j++){
+                    mat[row][j]=0;
+                }
             }
-            
-            for (int j = 0; j < n; ++j) {
-                matrix[row][j] = 0; // Zero out the row
-            }
+            // else{
+            //     mat[row][col]=1;
+            // }
         }
+        matrix=mat;
     }
 };
