9b16c7aa71c038c27300bd2ba27d00912e9cd919
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-08-13 15:42:11 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.64%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> arr, int row, int col, vector<vector<int>>& dp){
-        int m = arr.size();
-        int n = arr[0].size();
-        if(arr[m-1][n-1]==1){
-            return 0;
-        }
-        if(row==m-1 && col==n-1){
-            return 1;
-        }
-        if(row>=m || col>=n || arr[row][col]==1 ){
-            return 0;
-        }
-        if(dp[row][col]!=-1){
-            return dp[row][col];
-        }
-        int ways=0;
-        // if(row>=0 && row<m && col>=0 && col<n){
-            // if(arr[row][col]!=1){
-                //right ans bottom
-                ways = solve(arr,row+1,col,dp)+solve(arr,row,col+1,dp);
-            // }
-            
-        // }
-        return dp[row][col]=ways;
+    int f(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>> &dp){
+        if(i >= 0 && j >= 0 && obstacleGrid[i][j] == 1) return 0;
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = f(i-1, j, obstacleGrid, dp);
+        int left = f(i, j-1, obstacleGrid, dp);
+
+        return dp[i][j] = up+left;
     }
-    
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
-        return solve(obstacleGrid,0,0,dp);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(m-1, n-1, obstacleGrid, dp);
     }
 };
