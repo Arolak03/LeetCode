3eddd78829084067958cb5618a6a2a1498a75e43
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-08-13 16:28:43 +0530

    Time: 4 ms (65.08%), Space: 12 MB (60.66%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-   void nextPermutation(vector<int>& nums) {
-
-        // Using the in-built function
-        // next_permutation(nums.begin(),nums.end());
-
-        int idx=-1;
-        for(int i=nums.size()-2;i>=0;i--){
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int index=-1;
+        for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                idx=i;
+                index=i;
                 break;
             }
         }
-        if(idx!=-1){
-            for(int i=nums.size()-1;i>=0;i--){
-                if(nums[i]>nums[idx]){
-                    swap(nums[i],nums[idx]);
+        if(index!=-1){
+            for(int j=n-1;j>=0;j--){
+                if(nums[j]>nums[index]){
+                    swap(nums[index],nums[j]);
                     break;
                 }
             }
+        
+        // reverse(nums.begin()+index+1,nums.end());
         }
-        reverse(nums.begin()+idx+1,nums.end());
+        // swap(nums[n-1],nums[n-2]);
+        reverse(nums.begin()+index+1,nums.end());
     }
 };
