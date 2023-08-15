0f6548378354c3422bcc1fe58a0b326d9b2f83d2
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-08-15 22:07:48 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (40.88%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int index1 = 0;
-        int index2 = 0;
-        vector<int> merged;
-
-        while (index1 < m && index2 < n) {
-            if (nums1[index1] < nums2[index2]) {
-                merged.push_back(nums1[index1]);
+        int index1=0;
+        int index2=0;
+        vector<int> newone;
+        while(index1<m && index2<n){
+            if(nums1[index1]<nums2[index2]){
+                newone.push_back(nums1[index1]);
                 index1++;
-            } else {
-                merged.push_back(nums2[index2]);
-                index2++;
+                // cout<<index1<<endl;
             }
+            else{
+                newone.push_back(nums2[index2++]);}
         }
-
-        while (index1 < m) {
-            merged.push_back(nums1[index1]);
-            index1++;
+       
+         while(index1<m ){
+            newone.push_back(nums1[index1++]);
         }
-
-        while (index2 < n) {
-            merged.push_back(nums2[index2]);
-            index2++;
+         while(index2<n){
+            newone.push_back(nums2[index2++]);
         }
-
-        nums1 = merged;
+       nums1=newone;
     }
 };
