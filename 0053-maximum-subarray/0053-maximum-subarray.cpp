ee41984dd2a489bd25915c599a38733c251154a6
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-08-15 09:41:00 +0530

    Time: 81 ms (88.99%), Space: 67.7 MB (81.28%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     
-    int solve(vector<int>& nums, int n, int i){
-        long long maxi=INT_MIN;
-        long long sum=0;
-        if(i>=n){
-            return 0;
-        }
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            if(sum>maxi){
-                maxi=sum;
-            }
-            if(sum<0){
-                sum=0;
-            }
-        }
-        return maxi;
+    int solve(vector<int>& nums, int n) {
+    // int n = nums.size();
+    if (n == 0) {
+        return 0;
     }
+    
+    int max_ending_here = nums[0];
+    int max_so_far = nums[0];
+    
+    for (int i = 1; i < n; ++i) {
+        max_ending_here = max(nums[i], nums[i] + max_ending_here);
+        max_so_far = max(max_so_far, max_ending_here);
+    }
+    
+    return max_so_far;
+}
     
     int maxSubArray(vector<int>& nums) {
        int n = nums.size();
-        return solve(nums,n,0);
+        return solve(nums,n);
     }
 };
