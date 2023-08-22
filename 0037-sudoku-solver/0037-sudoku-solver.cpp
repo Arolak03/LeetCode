71a42285e8ffdbd21a77bb467dac9d490192c8fa
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-08-23 00:31:19 +0530

    Time: 22 ms (84.20%), Space: 6.3 MB (66.28%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -19,11 +19,15 @@ public:
     
     bool solve(vector<vector<char>>& board){
         int n = board[0].size();
+        
         for(int row=0;row<n;row++){
             for(int col=0;col<n;col++){
+                
                 if(board[row][col]=='.'){
+                    
                     for(int i=1;i<=9;i++){
                         char val = i + '0';
+                        
                         if(isSafe(row,col,board,val)){
                             board[row][col]=val;
                             //recursion
