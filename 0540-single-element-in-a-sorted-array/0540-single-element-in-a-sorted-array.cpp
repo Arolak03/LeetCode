59fbc1ca7768b6ad499dec7c2e9b2c897ca0a441
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-08-23 09:35:47 +0530

    Time: 14 ms (96.62%), Space: 22.4 MB (8.66%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int n =nums.size();
+    int singleNonDuplicate(vector<int>& arr) {
+        int n =arr.size();
         if(n==1){
-            return nums[0];
+            return arr[0];
         }
-        if(nums[0]!=nums[1]){
-            return nums[0];
+        if(arr[0]!=arr[1]){
+            return arr[0];
         }
-        if(nums[n-1]!=nums[n-2]){
-            return nums[n-1];
+        if(arr[n-1]!=arr[n-2]){
+            return arr[n-1];
         }
-        int low=1;
-        int high=n-2;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }
-            else if(mid%2==1){
-                if(nums[mid]!=nums[mid-1]){
-                    //left half
-                    high=mid;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-            else if(mid%2==0){
-                if(nums[mid]!=nums[mid-1]){
-                    //left half
-                    low=mid+1;
-                }
-                else{
-                    high=mid;
-                }
-            }
+       int low = 1, high = n - 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if arr[mid] is the single element:
+        if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
+            return arr[mid];
         }
-        return -1;
+
+        //we are in the left:
+        if ((mid % 2 == 1 && arr[mid] == arr[mid - 1])
+                || (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
+            //eliminate the left half:
+            low = mid + 1;
+        }
+        //we are in the right:
+        else {
+            //eliminate the right half:
+            high = mid - 1;
+        }
+    }
+
+    // dummy return statement:
+    return -1;
+
     }
 };
