bee779c5914c996e1fee9cc9a785258db205462a
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-08-25 15:20:09 +0530

    Time: 8 ms (14.70%), Space: 8.2 MB (45.28%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> freq;
-        for(auto nums: arr){
-            freq[nums]++;
+        vector<int> freq;
+        int i = 0;
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        while (i < n) {
+            int count = 1; // Initialize count to 1 for the current element.
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            cout<<count<<endl;
+            freq.push_back(count);
+            i+=count;
         }
-        unordered_set<int> unique;
-        for(auto &num: freq){
-            if(unique.count(num.second)>0){
+
+        sort(freq.begin(), freq.end());
+        cout<<freq.size();
+        for (int i = 1; i < freq.size(); i++) {
+            cout<<freq[i];
+            if (freq[i] == freq[i - 1]) {
                 return false;
             }
-            unique.insert(num.second);
         }
         return true;
     }
