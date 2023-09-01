929b8fb1bb9847ea1ee687c8ff1a3b90a1598e1f
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-09-01 20:28:46 +0530

    Time: 9 ms (71.96%), Space: 11.1 MB (86.30%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    
-    void dfs(int sum, vector<int>& ans, int n){
-        if(sum>=n){
-            return;
-        }
-        for(int i=0;i<=9;i++){
-            if(sum*10+i <= n){
-                ans.push_back(sum*10 + i);
-                dfs(sum*10 + i,ans,n);
-            }
-            
-        }
-    }
-    
     vector<int> lexicalOrder(int n) {
-        int sum =0;
-        vector<int> ans;
-        // 1 s start hone wale sare
-        for(int i=1;i<=9;i++){
-            if(sum*10+i<=n){
-                ans.push_back(sum*10+i);
-                dfs(sum*10+i,ans,n);
+       
+        vector<int> result;
+        int current = 1;
+        
+        for (int i = 1; i <= n; ++i) {
+            result.push_back(current);
+            
+            if (current * 10 <= n) {
+                current *= 10;
+            } else if (current % 10 != 9 && current + 1 <= n) {
+                current++;
+            } else {
+                while ((current / 10) % 10 == 9) {
+                    current /= 10;
+                }
+                current = current / 10 + 1;
             }
         }
-        return ans;
         
+        return result;
     }
 };
