f019547f190886aaba5d2698c2ff9b6d5b2b2449
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-09-03 02:52:15 +0530

    Time: 16 ms (38.19%), Space: 22.5 MB (52.86%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -11,46 +11,37 @@
  */
 class Solution {
 public:
+  
     vector<double> averageOfLevels(TreeNode* root) {
-        vector<vector<int>> op;
-        // if(root==NULL){
-        //     return NULL;
-        // }
-        queue<TreeNode*> q;
-        // stack<vector<int>> s;
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            vector<int> ans;
-            while(size--){
-                TreeNode* temp = q.front();
-                q.pop();
-                if(temp->left){
-                    q.push(temp->left);
-                    
-                }
-                if(temp->right){
-                    q.push(temp->right);
-                }
-                ans.push_back(temp->val);
-            }
-            op.push_back(ans);
-            // s.push(ans);
+        vector<double> avg;
+        if (root == nullptr) {
+            return avg;
         }
         
-        // op.push_back(stack);
-        // reverse(op.begin(), op.end());
-        // return op;
-        vector<double> avg;
-        for(int i=0;i<op.size();i++){
-            double sum=0;
-            for(int j=0;j<op[i].size();j++){
-                sum+=op[i][j];
-            }
-            sum/=op[i].size();
-            avg.push_back(sum);
-        }
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int size = q.size();
+            double levelSum = 0.0;
             
+            for (int i = 0; i < size; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                levelSum += temp->val;
+                
+                if (temp->left) {
+                    q.push(temp->left);
+                }
+                
+                if (temp->right) {
+                    q.push(temp->right);
+                }
+            }
+            
+            avg.push_back(levelSum / size);
+        }
+        
         return avg;
     }
 };
