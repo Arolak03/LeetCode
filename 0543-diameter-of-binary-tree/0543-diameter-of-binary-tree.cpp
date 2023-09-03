052652c8fe4c277792439ff0fd05399d7d82104b
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-09-03 15:23:41 +0530

    Time: 6 ms (80.04%), Space: 20.2 MB (82.60%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,55 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
-
-
 class Solution {
+public:
     
-//     private:
-    
-//     int height(TreeNode* root){
-//     if(root==NULL){
-//         return 0;
-//     }
-//     int left = height(root -> left);
-//     int right = height(root -> right);
-//     int ans = max(left, right)+1;
-//     return ans;
-// }
-    
-    public:
-    
-    pair<int, int> solve(TreeNode* root){
+    int solve(TreeNode* root, int& diam){
         if(root==NULL){
-            pair<int, int> p = make_pair(0,0);
-            return p;
+            return 0;
         }
-        pair<int, int> left = solve(root->left);
-        
-        pair<int, int> right = solve(root -> right);
-        int op1=left.first;
-        int op2=right.first;
-        int op3=left.second+right.second;
-        pair<int, int> ans;
-        
-        ans.second = max(left.second, right.second)+1;
-        ans.first = max(op1, max(op2,op3) );
-        return ans;
-        
+        int lh = solve(root->left,diam);
+        int rh = solve(root->right,diam);
+        diam = max(diam,lh+rh);
+        return 1+max(lh,rh);
     }
     
     
     int diameterOfBinaryTree(TreeNode* root) {
-        // if(root==NULL){
-        //     return 0;
-        // }
-        // int left = diameterOfBinaryTree(root->left);
-        // int right = diameterOfBinaryTree(root->right);
-        // int both = height(root->left) + height(root -> right) ;
-        // int ans = max(left, max(right, both));
-        // return ans;
-        return solve(root).first;
+        int diam =0 ;
+        solve(root, diam);
+        return diam;
     }
 };
