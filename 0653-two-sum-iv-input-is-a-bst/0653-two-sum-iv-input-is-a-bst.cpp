a78861521bf9b2888be2e31dd9fce17e0f16b9cf
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-09-07 22:58:59 +0530

    Time: 23 ms (94.85%), Space: 36.1 MB (96.83%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,49 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    
-    void solve(TreeNode* root, vector<int>& ans){
-        if(root==NULL)return;
-        
-        solve(root->left,ans);
-        ans.push_back(root->val);
-        solve(root->right,ans);
-    }
     bool findTarget(TreeNode* root, int k) {
-        vector<int> ans;
-        solve(root,ans);
-        // int ind = ans.size();
-        // for(int i=0;i<ind-1;i++){
-        //     for(int j=i+1;j<ind;j++){
-        //         if(ans[i]+ans[j]==k ){
-        //             return true;
-        //         }
-        //     }
-        // }
-        int s =0;
-        int e=ans.size()-1;
-        while(s<e){
-        int sum=ans[e]+ans[s];
-            if(sum==k){
-                return true;
-            }
-            if(sum>k){
-                e=e-1;
-            }
-            else{
-                s=s+1;
-            }
-        }
-        return false;
+        return dfs(root, root,  k);
+    }
+    
+    bool dfs(TreeNode* root,  TreeNode* cur, int k){
+        if(cur == NULL)return false;
+        return search(root, cur, k - cur->val) || dfs(root, cur->left, k) || dfs(root, cur->right, k);
+    }
+    
+    bool search(TreeNode* root, TreeNode *cur, int value){
+        if(root == NULL)return false;
+        return (root->val == value) && (root != cur) 
+            || (root->val < value) && search(root->right, cur, value) 
+                || (root->val > value) && search(root->left, cur, value);
     }
 };
