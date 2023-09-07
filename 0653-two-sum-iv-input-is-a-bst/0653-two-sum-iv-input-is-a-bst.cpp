cfeeef6217d7a7d9f5b69c8eb12697a4703d4a3f
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-09-07 22:56:08 +0530

    Time: 24 ms (91.98%), Space: 36.7 MB (74.91%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -14,19 +14,34 @@ public:
     
     void solve(TreeNode* root, vector<int>& ans){
         if(root==NULL)return;
-        ans.push_back(root->val);
+        
         solve(root->left,ans);
+        ans.push_back(root->val);
         solve(root->right,ans);
     }
     bool findTarget(TreeNode* root, int k) {
         vector<int> ans;
         solve(root,ans);
-        int ind = ans.size();
-        for(int i=0;i<ind-1;i++){
-            for(int j=i+1;j<ind;j++){
-                if(ans[i]+ans[j]==k ){
-                    return true;
-                }
+        // int ind = ans.size();
+        // for(int i=0;i<ind-1;i++){
+        //     for(int j=i+1;j<ind;j++){
+        //         if(ans[i]+ans[j]==k ){
+        //             return true;
+        //         }
+        //     }
+        // }
+        int s =0;
+        int e=ans.size()-1;
+        while(s<e){
+        int sum=ans[e]+ans[s];
+            if(sum==k){
+                return true;
+            }
+            if(sum>k){
+                e=e-1;
+            }
+            else{
+                s=s+1;
             }
         }
         return false;
