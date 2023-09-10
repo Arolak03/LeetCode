6e350910bf7f5b2eee9bc1714aea0c4e197ddc52
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-09-10 15:57:04 +0530

    Time: 4 ms (67.15%), Space: 13.7 MB (40.27%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,26 +11,35 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& preorder){
-        if(preorder.size()==0)return NULL;
-        TreeNode* root= new TreeNode(preorder[0]);
-        // if(preorder.size()==1)return root;
-        vector<int> left, right;
-        for(int i=0;i<preorder.size();i++){
-            if(preorder[i]>preorder[0]){
-                right.push_back(preorder[i]);
-            }
-            else if(preorder[i]<preorder[0]){
-                left.push_back(preorder[i]);
-            }
+    // TreeNode* solve(vector<int>& preorder){
+    //     if(preorder.size()==0)return NULL;
+    //     TreeNode* root= new TreeNode(preorder[0]);
+    //     // if(preorder.size()==1)return root;
+    //     vector<int> left, right;
+    //     for(int i=0;i<preorder.size();i++){
+    //         if(preorder[i]>preorder[0]){
+    //             right.push_back(preorder[i]);
+    //         }
+    //         else if(preorder[i]<preorder[0]){
+    //             left.push_back(preorder[i]);
+    //         }
+    //     }
+    TreeNode* solve(vector<int>& preorder, int& i, int bound){
+        if(i==preorder.size() || preorder[i]>bound){
+            return NULL;
         }
-        root->left=solve(left);
-        root->right=solve(right);
+        TreeNode* root = new TreeNode(preorder[i++]);
+        
+    
+    
+        root->left=solve(preorder,i, root->val);
+        root->right=solve(preorder,i, bound);
         return root;
     
     }
     
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return solve(preorder);
+        int i=0;
+        return solve(preorder, i, INT_MAX);
     }
 };
