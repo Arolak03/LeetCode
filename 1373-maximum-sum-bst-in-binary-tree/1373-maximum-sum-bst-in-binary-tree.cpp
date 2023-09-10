c361ba406ced7e0724d30a6745f8fb27be0a452b
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-09-10 19:40:41 +0530

    Time: 142 ms (83.73%), Space: 114.5 MB (61.99%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class info{
+    public:
+    int maxi;
+    int mini;
+    bool isBST;
+    int sum;
+};
+info solve(TreeNode*root, int &ans){
+    if(root==NULL)
+        return {INT_MIN,INT_MAX,true,0};
+    
+    info left = solve(root->left,ans);
+    info right = solve(root->right,ans);
+    info currNode;
+    
+    currNode.sum=root->val + left.sum + right.sum;
+    currNode.maxi=max(root->val,right.maxi);
+    currNode.mini = min(root->val,left.mini);
+    // cout<<currNode.sum<<endl;
+    //chreck bst
+    if(root->val>left.maxi && root->val<right.mini && left.isBST && right.isBST){
+        currNode.isBST = true;
+        
+    }
+    else{
+        currNode.isBST = false;
+    }
+    if(currNode.isBST){
+        ans=max(ans,currNode.sum);
+    }
+     // cout<<currNode.sum<<endl;
+    return currNode;
+  
+}
+
+class Solution {
+public:
+    int maxSumBST(TreeNode* root) {
+        int ans = 0;
+        info temp = solve(root,ans);
+        cout<<ans;
+        return ans;
+    }
+};
