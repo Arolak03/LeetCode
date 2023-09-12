a3bf4e72fdbb687ffe5c13a7d30b4216e179513b
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-09-13 02:41:05 +0530

    Time: 63 ms (98.13%), Space: 96.1 MB (96.38%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
+    int DIR[5] = {0, 1, 0, -1, 0};
     int islandPerimeter(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n = grid[0].size();
-        int perimeter=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    // boundaries
-                    if(i==0 || grid[i-1][j]==0){
-                        perimeter+=1;
-                    }
-                    //botoom
-                    if(i==m-1 || grid[i+1][j]==0){
-                        perimeter+=1;
-                    }
-                    if(j==0 || grid[i][j-1]==0){
-                        perimeter+=1;
-                    }
-                    if(j==n-1 || grid[i][j+1]==0){
-                        perimeter+=1;
-                    }
+        int row = grid.size(), col = grid[0].size(), perimeter = 0;
+        for (int r = 0; r < row; ++r) {
+            for (int c = 0; c < col; ++c) {
+                if (grid[r][c] == 0) continue; // Skip water cell
+                perimeter += 4;
+                for (int i = 0; i < 4; ++i) {
+                    int nr = r + DIR[i], nc = c + DIR[i+1];
+                    if (nr < 0 || nr == row || nc < 0 || nc == col || grid[nr][nc] == 0) continue;
+                    perimeter -= 1;
                 }
             }
         }
