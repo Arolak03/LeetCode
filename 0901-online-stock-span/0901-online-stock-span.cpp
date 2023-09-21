0c3ce498f2f8111e8be9c104f0a674cb35b6464d
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-09-21 15:55:56 +0530

    Time: 184 ms (79.88%), Space: 84.7 MB (21.53%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -5,6 +5,7 @@ public:
     vector<int> prices;
     unordered_map<int, int>mpp;
     stack<int> s;
+    stack<pair<int, int>>st;
     StockSpanner() {
         
     }
@@ -12,26 +13,33 @@ public:
     int next(int price) {
         // stack<int> s;
         // int count=0;
-        prices.push_back(price);
-        // for(int i=0;i<prices.size();i++){
-        //     s.push(prices[i]);
+        // prices.push_back(price);
+        // // for(int i=0;i<prices.size();i++){
+        // //     s.push(prices[i]);
+        // // }
+        // //     while(!s.empty() && s.top()<=price){
+        // //         count++;
+        // //         s.pop();  
+        // //     }
+        // //     if(s.empty() && count==0){
+        // //         count=1;
+        // //     }
+        // // else if(!s.empty() && count==0)count=1;
+        // // while(!s.empty())s.pop();
+        // int count=1;
+        // for(int i=prices.size()-2;i>=0;i--){
+        //     if(prices[i]<=price)count++;
+        //     else break;
         // }
-        //     while(!s.empty() && s.top()<=price){
-        //         count++;
-        //         s.pop();  
-        //     }
-        //     if(s.empty() && count==0){
-        //         count=1;
-        //     }
-        // else if(!s.empty() && count==0)count=1;
-        // while(!s.empty())s.pop();
-        int count=1;
-        for(int i=prices.size()-2;i>=0;i--){
-            if(prices[i]<=price)count++;
-            else break;
-        }
-        return count;
+        // return count;
         
+        int count=1;
+        while(!st.empty() && st.top().first<=price){
+            count+=st.top().second;
+            st.pop();
+        }
+        st.push({price,count});
+        return count;
     }
 };
 
