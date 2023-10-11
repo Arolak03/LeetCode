73237be7bbd9d3189472509b411847de7c64d050
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-11 23:06:25 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (21.28%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     
-    void solve(int k,int index, int n,vector<int>& arr, vector<vector<int>>& ans, vector<int>& op){
+    void solve(int k,int i, int n, vector<vector<int>>& ans, vector<int>& op){
         if(n==0 && k==0){
             ans.push_back(op);
             return; 
         }
-        if(index>=arr.size() || k<0 || n<0)return;
+        if(i>9 || k<0 || n<0)return;
         //include
-        op.push_back(arr[index]);
-        solve(k-1,index+1,n-arr[index],arr,ans,op);
-        op.pop_back();
-        solve(k,index+1,n,arr,ans,op);
+        // for(int i=1;i<=9;i++){
+            op.push_back(i);
+            solve(k-1,i+1,n-i,ans,op);
+            op.pop_back();
+            solve(k,i+1,n,ans,op);
+        // }
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
         vector<int> op;
-        vector<int> arr;
-        for(int i=1;i<=9;i++)arr.push_back(i);
+        // vector<int> arr;
+        // for(int i=1;i<=9;i++)arr.push_back(i);
         
-        solve(k,0,n,arr,ans,op);
+        solve(k,1,n,ans,op);
         return ans;
     }
 };
