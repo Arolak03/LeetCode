9cedc39e19b3c83822e13aba1327b0ce1d7a14f7
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-14 18:07:34 +0530

    Time: 2 ms (62.32%), Space: 8 MB (54.64%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     
-    void solve(vector<vector<int>>& ans, int index, vector<int>& nums){
-        if(index==nums.size()){
-            ans.push_back(nums);
+    void solve(vector<vector<int>>& ans, vector<int>& nums,int freq[],vector<int>& op, int index){
+        if(op.size()==nums.size()){
+            ans.push_back(op);
             return;
         }
         // daalna
-        for(int i=index;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
             // agr dala nhi h 
-            // if(!freq[i]){
-            //     op.push_back(nums[i]);
-            //     freq[i]=1;
-            //     solve(ans,op,freq,nums);
-            //     freq[i]=0;
-            //     op.pop_back();
-            // }
-            swap(nums[i],nums[index]);
-            solve(ans,index+1,nums);
-            swap(nums[i],nums[index]);
+            if(!freq[i]){
+                op.push_back(nums[i]);
+                freq[i]=1;
+                solve(ans,nums,freq,op,index+1);
+                freq[i]=0;
+                op.pop_back();
+            }
+            // swap(nums[i],nums[index]);
+            // solve(ans,index+1,nums);
+            // swap(nums[i],nums[index]);
         }
     }
     
@@ -27,12 +27,12 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         int n =nums.size();
-        // vector<int> op;
-        // int freq[n];
-        // for(int i=0;i<n;i++){
-            // freq[i]=0;
-        // }
-        solve(ans,0, nums);
+        vector<int> op;
+        int freq[n];
+        for(int i=0;i<n;i++){
+            freq[i]=0;
+        }
+        solve(ans, nums,freq,op,0);
         return ans;
     }
 };
