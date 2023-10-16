0de1b750f8b16c87519ed0535d9953e0d698f368
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-16 16:08:02 +0530

    Time: 360 ms (83.35%), Space: 165.7 MB (69.56%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,65 +1,36 @@
 class LRUCache {
 public:
-    // keys k liye
-    map<int, int> m;
-    //address k liuye
-    map<int,list<int>::iterator> address;
-    //nodes insert delete
-    list<int> l;
-    int cap;
-    //jitni max gh
+    unordered_map<int, list<pair<int, int>>::iterator> mpp;
+    list<pair<int, int>> lrulist;  // key-value pairs
     int size;
-    // jitnme presently h
+
     LRUCache(int capacity) {
-        cap=capacity;
-        size=0;
-        //initially
+        size = capacity;
     }
-    
+
     int get(int key) {
-        // ab age key mile hi na
-        if(m.find(key)==m.end()){
+        if (mpp.find(key) != mpp.end()) {
+            auto lrulistIterator = mpp[key];
+            lrulist.splice(lrulist.begin(), lrulist, lrulistIterator);
+            return lrulistIterator->second;
+        } else {
             return -1;
         }
-        //mil jaaye to kaha milegi waha s hata kr front m daaln h
-        list<int>::iterator it = address[key];
-        //yaha s hatao l s hatao address s hatao
-        l.erase(it);
-        address.erase(key);
-        //aage daalo
-        l.push_front(key);
-        address[key]=l.begin();
-        return m[key];
     }
-    
+
     void put(int key, int value) {
-        //if old key already present then wo wali hataao
-        if(m.find(key)!=m.end()){
-            l.erase(address[key]);
-            address.erase(key);
-            m.erase(key);
-            size--;
+        if (mpp.find(key) != mpp.end()) {
+            auto lrulistIterator = mpp[key];
+            lrulistIterator->second = value;  // Update the value
+            lrulist.splice(lrulist.begin(), lrulist, lrulistIterator);
+        } else {
+            if (lrulist.size() >= size) {
+                int toRemove = lrulist.back().first;
+                lrulist.pop_back();
+                mpp.erase(toRemove);
+            }
+            lrulist.push_front({key, value});
+            mpp[key] = lrulist.begin();
         }
-        //ab jb puraani hatali to nyi daalo udhr
-        //lekin agr capacity reached to last wali hatado
-        if(cap==size){
-            int k = l.back();
-            l.pop_back();
-            address.erase(k);
-            m.erase(k);
-            size--;
-        }
-        l.push_front(key);
-        address[key]=l.begin();
-        m[key]=value;
-        size++;
-        
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
