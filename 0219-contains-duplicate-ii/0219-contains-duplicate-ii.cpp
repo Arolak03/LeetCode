b5bf4753b1a66af55cf826853abc09b00506eaa6
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-17 23:34:38 +0530

    Time: 124 ms (77.44%), Space: 77.7 MB (42.34%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,36 +1,14 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-//         int n = nums.size();
-//         int i1=0;
-//         int i2=0;
-//         for(int i=0;i<n;i++){
-//             for(int j=n-1;j>i;j--){
-//                 if(nums[i]==nums[j]){
-//                     i1=i;
-//                     i2=j;
-//                     break;
-//                 }
-//             }
-//         }
-//         if(abs(i1-i2)<=k)return true;
-                   
-//         return false;
-//         unordered_map<pair<int, int>. int> mpp;
-//         for(int i=0;i<nums.size();i++){
-//             mpp({nums[i],i})++;
-//         }
-        vector<pair<int, int>> v;
+        unordered_map<int, int>mpp;
+        int n = nums.size();
         for(int i=0;i<nums.size();i++){
-            v.push_back({nums[i],i});
+            if(mpp.count(nums[i])){
+                if(abs(i-mpp[nums[i]])<=k)return true;
+            }
+            mpp[nums[i]]=i;
         }
-        sort(v.begin(),v.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(v[i].first==v[i+1].first && abs(v[i].second-v[i+1].second)<=k)return true;
-        }
-        
         return false;
-        
-        
     }
 };
