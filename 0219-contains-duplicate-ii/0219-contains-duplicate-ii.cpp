11b84ce382b2577971d9bd63f4aa8eb83c54989d
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-17 23:35:48 +0530

    Time: 123 ms (80.05%), Space: 77.8 MB (28.62%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -4,8 +4,8 @@ public:
         unordered_map<int, int>mpp;
         int n = nums.size();
         for(int i=0;i<nums.size();i++){
-            if(mpp.count(nums[i])){
-                if(abs(i-mpp[nums[i]])<=k)return true;
+            if(mpp.count(nums[i]) && abs(i-mpp[nums[i]])<=k){
+                return true;
             }
             mpp[nums[i]]=i;
         }
