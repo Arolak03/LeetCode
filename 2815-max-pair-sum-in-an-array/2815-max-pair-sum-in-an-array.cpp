6059a6488f21a368195354f40cb53cb3465300c6
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-26 02:49:19 +0530

    Time: 44 ms (32.63%), Space: 72.4 MB (5.45%) - LeetHub

--- 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.cpp
+++ 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.cpp
@@ -1,27 +1,35 @@
 class Solution {
-public:
-    int maxDigit(int dig){
-        int digi=-1;
-        while(dig>0){
-            digi=max(digi,dig%10);
-            dig/=10;
+private:
+    int findMax(int number){
+        int maxDigit = INT_MIN;
+        while(number > 0){
+            maxDigit = max(maxDigit, number % 10);
+            number /= 10;
         }
-        return digi;
+        return maxDigit;
     }
     
+public:
     int maxSum(vector<int>& nums) {
-        
-        int n =nums.size();
-        int ans=INT_MIN;
-        vector<int> v(10,INT_MIN);
-        for(int i=0;i<n;i++){
-            // int num=nums[i];
-            int digit=maxDigit(nums[i]);
-            ans=max(ans,nums[i]+v[digit]);
-            v[digit]=max(v[digit],nums[i]);
+        // Intution
+        // The idea in here is really very simple. We will find the maximum digit of a number and then add it to the map
+        unordered_map<int, vector<int>> mp;
+        for(auto &num : nums){
+            int digit = findMax(num);
+            if(mp.find(digit) == mp.end()){
+                mp[digit] = {num};
+            }
+            else{
+                mp[digit].push_back(num);
+            }
         }
-        cout<<INT_MIN;
-        if(ans<0)return -1;
-        else return ans;
+        int answer = -1;
+        for(auto itr = mp.begin(); itr != mp.end(); itr++){
+            vector<int> curr = itr->second;
+            sort(curr.begin(), curr.end());
+            if(curr.size() == 1) continue;
+            answer = max(answer, curr[curr.size() - 1] + curr[curr.size() - 2]);
+        }
+        return answer;
     }
 };
