0002d0dcb3a6018c110427be49e8cabe160424de
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-26 02:45:21 +0530

    Time: 24 ms (88.26%), Space: 68.4 MB (40.54%) - LeetHub

--- 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.cpp
+++ 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int maxSum(vector<int>& nums) {
-        int n  =nums.size();
-        int ans=INT_MIN;
-        
-        for(int i=0;i<n-1;i++){
-            string s1=to_string(nums[i]);
-            sort(s1.begin(), s1.end());
-            for (int j=i+1;j<n;j++){
-                string s2=to_string(nums[j]);
-                sort(s2.begin(), s2.end());
-                if(s1[s1.length()-1]==s2[s2.length()-1])ans=max(ans,nums[i]+nums[j]);
-            }
-            
+    int maxDigit(int dig){
+        int digi=-1;
+        while(dig>0){
+            digi=max(digi,dig%10);
+            dig/=10;
         }
-        return ans==INT_MIN?-1:ans;
+        return digi;
+    }
+    
+    int maxSum(vector<int>& nums) {
+        
+        int n =nums.size();
+        int ans=INT_MIN;
+        vector<int> v(10,INT_MIN);
+        for(int i=0;i<n;i++){
+            // int num=nums[i];
+            int digit=maxDigit(nums[i]);
+            ans=max(ans,nums[i]+v[digit]);
+            v[digit]=max(v[digit],nums[i]);
+        }
+        cout<<INT_MIN;
+        if(ans<0)return -1;
+        else return ans;
     }
 };
