a72c78af107c7a1072ef3d77950d63370ad39108
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-25 22:04:44 +0530

    Added solution - LeetHub

--- Palindromic Partitioning - GFG/palindromic-partitioning.cpp
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -9,30 +9,50 @@ using namespace std;
 
 class Solution{
 public:
-int palindromicPartition(string s) {
-    int n = s.length();
-    vector<vector<bool>> isPalindrome(n, vector<bool>(n, false));
-    vector<int> dp(n, INT_MAX);
 
-    for (int end = 0; end < n; end++) {
-        for (int start = 0; start <= end; start++) {
-            if (s[start] == s[end] && (end - start <= 1 || isPalindrome[start + 1][end - 1])) {
-                isPalindrome[start][end] = true;
-            }
+    bool isPalindrome(string s, int start, int end,vector<vector<int>>& check){
+       if(start>=end)return true;
+       if(check[start][end]!=-1)return check[start][end];
+    
+        if(s[start]==s[end] && isPalindrome(s,start+1,end-1,check)){
+            check[start][end]=1;
+            return true;
         }
+        check[start][end]=0;
+        return false;
+        
     }
 
-    for (int end = 0; end < n; end++) {
-        for (int start = 0; start <= end; start++) {
-            if (isPalindrome[start][end]) {
-                dp[end] = (start == 0) ? 0 : min(dp[end], dp[start - 1] + 1);
-            }
-        }
+    int solve(string s, int prev, int index,vector<vector<int>>& dp,vector<vector<int>>& check){
+        if(prev>=index || isPalindrome(s,prev,index,check))return 0;
+        if(dp[prev][index]!=-1)return dp[prev][index];
+        int ans=INT_MAX;
+        int count;
+        // for(int index=n-1;index>=1;index--){
+        //     for(int prev=1;prev<=n;prev++){
+                for(int i=prev;i<index;i++){
+                    if(!isPalindrome(s,prev,i,check)){
+                        continue;
+                    }
+                    count =solve(s,i+1,index,dp,check)+1;
+                    ans=min(count,ans);
+                }
+            // }
+        // }
+        
+        return dp[prev][index]=ans;
     }
 
-    return dp[n - 1];
-}
+    int palindromicPartition(string str)
+    {
+        int n = str.length();
+        vector<vector<int>> dp(str.length()+1,vector<int>(str.length()+1,-1));
+        vector<vector<int>> check(n+1, vector<int>(n+1,-1));
+        // code here
+        return solve(str,0,str.length()-1,dp,check);
+    }
 };
+
 
 //{ Driver Code Starts.
 
