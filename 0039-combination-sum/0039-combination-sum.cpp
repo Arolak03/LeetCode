bf86c33a158d8e42182ee5dee72ffdc9b277688c
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-31 11:09:41 +0530

    Time: 5 ms (56.52%), Space: 10.9 MB (83.54%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,25 @@
-class Solution {
-public:
+class Solution{
+    public:
+void solve(vector<int>& nums, int target, vector<int>& op, vector<vector<int>>& ans, int index) {
+    if (target == 0) {
+        ans.push_back(op);
+        return;
+    }
+    if (target < 0 || index == nums.size()) {
+        return;
+    }
     
-    void solve(vector<int>& nums, int target, vector<int>& op, vector<vector<int>>& ans, int index){
-        if(target==0){
-            ans.push_back(op);
-            return;
-        }
-        if(target<0 || index>=nums.size())return;
-        op.push_back(nums[index]);
-        solve(nums,target-nums[index],op,ans,index);
+    for (int i = index; i < nums.size(); i++) {
+        op.push_back(nums[i]);
+        solve(nums, target - nums[i], op, ans, i); // Use i to move to the next element
         op.pop_back();
-        solve(nums, target, op, ans, index + 1);
     }
-    
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>op;
-        vector<vector<int>> ans;
-        solve(candidates,target,op,ans,0);
-        return ans;
-    }
+}
+
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<int> op;
+    vector<vector<int>> ans;
+    solve(candidates, target, op, ans, 0);
+    return ans;
+}
 };
