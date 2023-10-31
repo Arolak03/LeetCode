5d5ef461425a52dc9a11943dfa3fc4fad44bd65a
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-31 11:57:50 +0530

    Time: 3 ms (93.77%), Space: 11.9 MB (28.90%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    
-    static bool compare(string a, string b){
-        
-        return a+b>b+a;
-    }
-        
     string largestNumber(vector<int>& nums) {
         vector<string> s;
-        int n =nums.size();
-        for(int i=0;i<nums.size();i++){
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
             s.push_back(to_string(nums[i]));
         }
-        sort(s.begin(),s.end(),compare);
-        // reverse(s.begin(), s.end());
+        
+        // Custom comparison function to compare two strings
+        auto compare = [](const string& a, const string& b) {
+            return a + b > b + a;
+        };
+        
+        // Sort using the custom comparison function
+        sort(s.begin(), s.end(), compare);
+        
+        // Concatenate the sorted strings
         string str;
-        for(int i=0;i<n;i++){
-            str+=s[i];
+        for (int i = 0; i < n; i++) {
+            str += s[i];
         }
-        // int x=stol(str);
-        // cout<<x;
-        // string ss=to_string(x);
-        // return ss;
-        if(str[0]=='0')return "0";
+        
+        // Check if the result should be "0"
+        if (str[0] == '0') {
+            return "0";
+        }
+        
         return str;
     }
 };
