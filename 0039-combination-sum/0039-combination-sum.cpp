a1b5a5db401dd36316e4ad8cd6e8a593eeae9980
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-10-31 10:58:31 +0530

    Time: 4 ms (75.22%), Space: 11.2 MB (61.49%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    
-    void solve(vector<int>& nums, int& target,int index, int sum, vector<vector<int>>& ans, vector<int>& op){
-        if(index==nums.size()){
-          
+    void solve(vector<int>& candidates, int target, vector<int>& op, vector<vector<int>>& ans, int index) {
+        if (target == 0) {
+            ans.push_back(op);
             return;
         }
-        if(sum>=target){
-              if(sum==target){
-                ans.push_back(op);
-            }
+        if (target < 0 || index >= candidates.size()) {
             return;
         }
         
-        op.push_back(nums[index]);
-        sum+=nums[index];
-        solve(nums,target,index,sum,ans,op);
-        
+        // Include the current candidate and make a recursive call
+        op.push_back(candidates[index]);
+        solve(candidates, target - candidates[index], op, ans, index);
         op.pop_back();
-        sum-=nums[index];
-        solve(nums,target,index+1,sum,ans,op);
+        
+        // Skip the current candidate and make a recursive call
+        solve(candidates, target, op, ans, index + 1);
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
         vector<int> op;
-        solve(candidates, target, 0,0, ans, op);
+        vector<vector<int>> ans;
+        solve(candidates, target, op, ans, 0);
         return ans;
     }
 };
