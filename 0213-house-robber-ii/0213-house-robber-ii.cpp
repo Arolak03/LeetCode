03b587aff5d99a52245b5eceec087f3c9abaf447
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-02 00:58:16 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (19.39%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,39 +1,48 @@
 class Solution {
 public:
-     int solve1(vector<int>& nums, int index, vector<int>& dp){
-         //last nhi aayega
-        if(index>=nums.size()-1)return 0;
-        if(dp[index]!=-1)return dp[index];
-        int include=nums[index]+solve(nums,index+2,dp);
-        int exclud=solve(nums,index+1,dp);
-        return dp[index]=max(include, exclud);
-    
+    int solve1(vector<int>& nums, int index, vector<int>& dp) {
+        // Base case: If the index is out of bounds, return 0
+        // cout<<index<<endl<<dp[0];
+        if (index >= nums.size()-1) {
+            // cout<<'*';
+            return 0;
+          
+        }
+        if (dp[index] != -1) {
+            // cout<<'#';
+            return dp[index];
+        }
+        int exclude=0;
+        int include = nums[index] + solve1(nums, index + 2, dp);
+        exclude = solve1(nums, index + 1, dp);
+        return dp[index] = max(include, exclude);
     }
-     int solve(vector<int>& nums, int index, vector<int>& dp){
-        if(index>=nums.size())return 0;
-        if(dp[index]!=-1)return dp[index];
-        int include=nums[index]+solve(nums,index+2,dp);
-        int exclud=solve(nums,index+1,dp);
-        return dp[index]=max(include, exclud);
-    
+
+    int solve(vector<int>& nums, int index, vector<int>& dp) {
+        // Base case: If the index is out of bounds, return 0
+        if (index >= nums.size()) return 0;
+        if (dp[index] != -1) return dp[index];
+        int exclude=0;
+        int include = nums[index] + solve(nums, index + 2, dp);
+        // int exclude = solve(nums, index + 1, dp);
+         exclude = solve(nums, index + 1, dp);
+        return dp[index] = max(include, exclude);
     }
+
     int rob(vector<int>& nums) {
-        vector<int> nums1;
-        if(nums.size()==1)return nums[0]; 
-        for(int i=0;i<nums.size()-1;i++){
-            nums1.push_back(nums[i]);
+        int ans=INT_MIN;
+        if(nums.size()<=3){
+            for(int i=0;i<nums.size();i++){
+                ans=max(ans,nums[i]);
+            }
+            return ans;
+                        
         }
-        vector<int> nums2;
-        
-        for(int i=1;i<nums.size();i++){
-            nums2.push_back(nums[i]);
-        }
-            
-        vector<int> dp1(nums.size(),-1);
-        vector<int> dp2(nums.size(),-1);
-        int a=solve(nums1,0,dp1);
-        int b=solve(nums2,0,dp2);
-        // cout<<a<<endl<<b;
-        return max(a,b);
+        vector<int> dp1(nums.size(), -1);
+        vector<int> dp2(nums.size(), -1);   
+        int a = solve1(nums, 0, dp1);
+        int b = solve(nums, 1, dp2);
+        cout<<a<<b;
+        return max(a, b);
     }
 };
