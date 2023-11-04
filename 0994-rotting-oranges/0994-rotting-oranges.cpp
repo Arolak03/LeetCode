1a8127fad0c5586b8c3d393a945ba74730b181e7
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-04 14:44:08 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (76.67%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -38,7 +38,13 @@ public:
             if(!rotten.empty())time++;
         }
               
-        return tot==cnt? time:-1;
+        for(int i =0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1)
+                return -1;
+            }
+        }
+        return time;
               
         
     }
