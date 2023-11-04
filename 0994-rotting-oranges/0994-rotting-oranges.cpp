01858e2a255eaf7c68b5fa6756612168752df9d7
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-04 14:43:05 +0530

    Time: 10 ms (21.08%), Space: 13.4 MB (58.24%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int cnt=0, tot=0;
+        int x=grid.size(), y=grid[0].size(), time=0;
+        queue<pair<int, int>>rotten;
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+                if(grid[i][j]!=0)tot+=1;
+                if(grid[i][j]==2){
+                    // tot+=1;
+                    rotten.push({i,j});
+                }
+            }
+        }
+        //rotten queue m h ab
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+        while(!rotten.empty()){
+            
+            //x and y coordinates;
+            // rotten.pop();
+            int k=rotten.size();
+            cnt+=k;
+            while(k--){
+                int m=rotten.front().first;
+                int n=rotten.front().second;
+                rotten.pop();
+                for(int i=0;i<4;i++){
+                    int mx=m+dx[i];
+                    int my=n+dy[i];
+                    
+                    if(mx<0 || my<0 || mx>=x || my>=y || grid[mx][my]!=1)continue;
+                    grid[mx][my]=2;
+                    rotten.push({mx,my});
+                }
+            }
+            if(!rotten.empty())time++;
+        }
+              
+        return tot==cnt? time:-1;
+              
+        
+    }
+};
