f5234749c5d801c0dffdbf8ca6e40a021a7780fb
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-08 22:38:04 +0530

    Time: 54 ms (91.59%), Space: 18.6 MB (53.80%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-        int longestStrChain(vector<string>& words) {
+    int longestStrChain(vector<string>& words) {
         int n = words.size();
-        if (n == 1) return 1;
-        sort(words.begin(), words.end(), [&](string& x, string& y) {
-            return x.size() < y.size(); 
+        if(n==1)return 1;
+        sort(words.begin(), words.end(), [&] (string& a, string& b){
+            return a.size()<b.size();
         });
-        vector<unordered_map<string, int>> mp(17);
-        for (auto& w : words)
-            mp[w.size()][w] = 1;
-
-        int ans = 0;
-
-        for (auto& w : words) {
-            int sz = w.size();
-            if (sz == 1) continue;
-            for (int i = 0; i < sz; i++) {
-                string w2 = w.substr(0, i) + w.substr(i+1); 
-                if (mp[sz-1].count(w2) != 0) {
-                    mp[sz][w] = max(mp[sz][w], mp[sz-1][w2] + 1);
+             
+        //vector jiske index uske number of letters k hoga 
+        vector<unordered_map<string, int>>mpp(17);
+        for(auto w: words){
+            mpp[w.size()][w]=1;
+        } 
+        int ans=0;
+        for(auto w: words){
+            int sz=w.size();
+            if(sz==1)continue;
+            for(int i=0;i<sz;i++){
+                string m = w.substr(0,i)+w.substr(i+1);
+                if(mpp[sz-1].count(m)){
+                    mpp[sz][w]=max(mpp[sz][w], 1+mpp[sz-1][m]);
                 }
             }
-            ans = max(ans, mp[sz][w]);
+            ans=max(ans,mpp[sz][w]);
         }
-
         return ans;
-
+        
     }
 };
