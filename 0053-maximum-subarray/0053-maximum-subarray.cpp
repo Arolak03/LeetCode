8f448053d2b0d6ac4a080f94cda09c3db5995fce
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-10 18:02:26 +0530

    Time: 87 ms (42.85%), Space: 68.1 MB (51.81%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,25 +1,53 @@
 class Solution {
 public:
     
-    int solve(vector<int>& nums, int n) {
-    // int n = nums.size();
-    if (n == 0) {
-        return 0;
-    }
-    
-    int max_ending_here = nums[0];
-    int max_so_far = nums[0];
-    
-    for (int i = 1; i < n; ++i) {
-        max_ending_here = max(nums[i], nums[i] + max_ending_here);
-        max_so_far = max(max_so_far, max_ending_here);
-    }
-    
-    return max_so_far;
-}
+//     int solve(int i, int j,vector<int>& nums){
+//         // if(index>=nums.size()){
+//         //     return 0;
+//         // }
+//         // int incl=nums[index]+solve(nums,index+1);
+//         // int excl=0+solve(nums,index+1);
+//         // return max(incl, excl);
+//         int ans=0;
+//         for(int m=i;m<=j;m++){
+//             ans+=nums[m];
+//         }
+//         return ans;
+        
+//     }
     
     int maxSubArray(vector<int>& nums) {
-       int n = nums.size();
-        return solve(nums,n);
+//         int n =nums.size();
+//         int maxi=INT_MIN;
+//         // for(int i=0;i<nums.size();i++){
+//         //     for(int j=i;j<n;j++){
+//         //         int newmax=solve(i,j,nums);
+//         //         maxi=max(maxi,newmax);
+//         //     }
+//         // }
+//         // return maxi;
+//         vector<int> dum(n);
+//         dum[0]=nums[0];
+//         for(int i=1;i<n;i++){
+//             dum[i]=dum[i-1]+nums[i];
+//         }
+//         for(int i=n-1;i>=0;i--){
+//             int ans=dum[i];
+//             for(int j=i-1;j>=0;j--){
+//                 maxi=max(ans,maxi);
+//                 ans -= dum[j];
+                
+//             }
+//         }
+//         return maxi;
+        
+        int sum=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            maxi=max(sum,maxi);
+            if(sum<0)sum=0;
+        }
+        return maxi;
     }
 };
