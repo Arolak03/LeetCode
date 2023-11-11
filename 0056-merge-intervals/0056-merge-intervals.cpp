0c650e1511e7e47a1f162e3763eda3b8c89373e9
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-11 10:43:28 +0530

    Time: 34 ms (40.43%), Space: 19.3 MB (91.94%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& arr) {
-        int n = arr.size();
-        sort(arr.begin(),arr.end());
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n =intervals.size();
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        for (int i = 0; i < n; i++) {
-        // if the current interval does not
-        // lie in the last interval:
-        if (ans.empty() || arr[i][0] > ans.back()[1]) {
-            ans.push_back(arr[i]);
+        for(int i=0;i<n;i++){
+            // int start=intervals[i][0];
+            // int end=intervals[i][1];
+            // if(!ans.empty() && ans.back()[1] >= end)continue;
+            // for(int j=i;j<n;j++){
+            //     if(intervals[j][0]<=end)end=max(end,intervals[j][1]);
+            //     else break;
+            // }
+            // ans.push_back({start,end});ans
+            if(ans.empty() || ans.back()[1]<intervals[i][0]){
+                ans.push_back(intervals[i]);
+            }
+            else {
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            }
         }
-        // if the current interval
-        // lies in the last interval:
-        else {
-            ans.back()[1] = max(ans.back()[1], arr[i][1]);
-        }
-    }
-    return ans;
+        return ans;
     }
 };
