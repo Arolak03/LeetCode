d09f119ef8437cbf8d9552e57a351d77a4bca01e
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-12 03:02:59 +0530

    Time: 13 ms (34.31%), Space: 14.2 MB (8.13%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m= matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> mat=matrix;
-        for(int i=0;i<m*n;i++){
-            int row = i/n;
-            int col = i%n;
-            if(matrix[row][col]==0){
-                //row same rkhna h 
-                for(int j=0;j<m;j++){
-                    mat[j][col]=0;
-                }
-                //col same
-                for(int j=0;j<n;j++){
-                    mat[row][j]=0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> visited = matrix;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<m; k++){
+                        visited[i][k] = 0;
+                    }
                 }
             }
-            // else{
-            //     mat[row][col]=1;
-            // }
         }
-        matrix=mat;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<n; k++){
+                        visited[k][j] = 0;
+                    }
+                }
+            }
+        }
+        matrix=visited;
     }
 };
