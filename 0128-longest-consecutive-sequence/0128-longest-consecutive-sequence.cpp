deb0dc20dcc910ce0f4e8cb6b52ead8fcd5f5c1f
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-13 23:53:57 +0530

    Time: 1147 ms (13.97%), Space: 70.9 MB (24.04%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,40 +1,22 @@
 class Solution {
-  
-    private:
- int fun1(vector<int>& nums) {
-     
-    std::unordered_set<int> s(nums.begin(),nums.end());
-    int ans = 1;
-
-   while(s.size()>0) {
-       int maxi=1;
-       int ele=*(s.begin());  // TAKE ELEMENT
-       s.erase(ele);
-       int temp=ele-1;
-       while(s.count(temp)){ //LESSER 
-           maxi++;
-           s.erase(temp);
-           temp--;
-       }
-       temp=ele+1;
-        while(s.count(temp)){  //GREATER
-           maxi++;
-           s.erase(temp);
-           temp++;
-       }
-       ans=max(maxi,ans);
-    }
-
-    return ans;
-}
-// void fast(){
-//       std::ios_base::sync_with_stdio(false);
-//     std::cin.tie(nullptr);
-// }
-public:
+  public:
     int longestConsecutive(vector<int>& nums) {
-        // fast();
-        if(nums.size()==0)return 0;
-        return fun1(nums);
+        unordered_set<int> set;
+        for(int num : nums){
+            set.insert(num);
+        }
+        int longestConsecutiveSequence = 0;
+        for(int num : nums){
+            if(set.find(num-1) == set.end()){
+                int currentNumber = num;
+                int currentConsecutiveSequence = 1;
+                while(set.find(currentNumber+1) != set.end()){
+                    currentNumber++;
+                    currentConsecutiveSequence++;
+                }
+                longestConsecutiveSequence = max(longestConsecutiveSequence, currentConsecutiveSequence);
+            }
+        }
+        return longestConsecutiveSequence;
     }
 };
