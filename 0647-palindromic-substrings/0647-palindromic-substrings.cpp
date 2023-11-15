fda39097e33fc0b465bcd87299a54c8626ac38f4
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-15 12:22:36 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (87.89%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,19 +1,20 @@
-class Solution{
-    public:
-    int countSubstrings(string& s) {
-        vector<vector<int>> mem(s.size(), vector<int>(s.size(), -1));
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.length();
         int count = 0;
-        for(int i = 0; i < s.size(); ++i) {
-            for(int j = i; j < s.size(); ++j) {
-                count += solve(mem, s, i, j);
+
+        for (int center = 0; center < 2 * n - 1; ++center) {
+            int left = center / 2;
+            int right = left + center % 2;
+
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                count++;
+                left--;
+                right++;
             }
         }
+
         return count;
-    }
-    
-    int solve(vector<vector<int>>& mem, string& s, int i, int j) {
-        if (i >= j) return 1;
-        if (mem[i][j] >= 0) return mem[i][j];
-        return mem[i][j] = s[i] == s[j] ? solve(mem, s, i+1, j-1) : 0;
     }
 };
