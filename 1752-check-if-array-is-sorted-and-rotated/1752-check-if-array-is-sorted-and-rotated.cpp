099deff38a5c7ae3de93161404fd24aab03d30f6
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-19 13:16:13 +0530

    Time: 0 ms (100.00%), Space: 9 MB (11.46%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -5,6 +5,7 @@ public:
         for(int i=0;i<nums.size()-1; i++){
             if(nums[i]>nums[i+1]){
                 cnt++;
+                if(cnt>1)return false;
             }
         }
         if(nums[nums.size()-1]>nums[0])cnt++;
