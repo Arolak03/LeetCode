7d8c706df7a7f460eb8fe5eaf5e1a11e9bda95cc
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-21 11:45:29 +0530

    Time: 132 ms (63.85%), Space: 70.9 MB (37.94%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,22 +1,63 @@
+
 class Solution {
-  public:
+public:
+    
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> set;
-        for(int num : nums){
-            set.insert(num);
-        }
-        int longestConsecutiveSequence = 0;
-        for(int num : nums){
-            if(set.find(num-1) == set.end()){
-                int currentNumber = num;
-                int currentConsecutiveSequence = 1;
-                while(set.find(currentNumber+1) != set.end()){
-                    currentNumber++;
-                    currentConsecutiveSequence++;
+        unordered_set<int> mpp(nums.begin(), nums.end());
+        if(nums.size()==0)return 0;
+        // for(int i=0;i<nums.size();i++){
+        //     mpp[nums[i]]++;
+        //     //each atleast one;
+        // }
+        int maxi=1;
+        for(auto it: mpp){
+            //jiska prev na ho
+            // int x=it.first;
+            if(mpp.find(it-1)==mpp.end()){
+                int count=1;
+                int x=it;
+                
+                while(mpp.find(x+1)!=mpp.end()){
+                    count++;
+                    x++;
                 }
-                longestConsecutiveSequence = max(longestConsecutiveSequence, currentConsecutiveSequence);
+                maxi=max(maxi,count);
             }
+            // maxi=max(maxi,mpp[it.first]);
         }
-        return longestConsecutiveSequence;
+        return maxi;
     }
 };
+
+
+
+
+
+// int longestConsecutive(vector<int>& nums) {
+
+//         if(nums.size() == 0)
+//         { return 0; }
+
+//         int longest = 1;
+//         int current = 1;
+
+//         unordered_set<int> sett(nums.begin(), nums.end());
+
+//         for(auto it: sett)
+//         {
+//             if(sett.find(it-1) == sett.end()) // previous element not in set
+//             {
+//                 current = 1;    // counter restart
+//                 int x = it;
+
+//                 while(sett.find(x+1) != sett.end()) // next element is present
+//                 {
+//                     x = x + 1;
+//                     current++;
+//                 }
+//                 longest = max(longest,current);
+//             }
+//         }
+//         return longest;
+//     }
+// };
