e4e6c0f8fe6b8fcd14dc152b95fa09cbf225e45d
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-11-27 19:52:16 +0530

    Time: 3 ms (53.12%), Space: 12.5 MB (27.16%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,38 +1,44 @@
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if (root == nullptr) {
-            return result;
-        }
+        vector<vector<int>> ans;
+        if(!root)return ans;
+        // int index=0;
         queue<TreeNode*> q;
+        bool left=true;
         q.push(root);
-        bool leftRight = true;
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> ans(size);
-            for (int i = 0; i < size; i++) {
-                TreeNode* frontNode = q.front();
+        while(!q.empty()){
+            int sz=q.size();
+            // if(left){
+            //     for(int i=0;i<sz;i++){
+            //         ans[index][i]=q.front()
+            //     }
+            // }
+            vector<int> op(sz);
+            for (int i = 0; i < sz; ++i) {
+                TreeNode* temp = q.front();
                 q.pop();
-                int index = leftRight ? i : size - i - 1;
-                ans[index] = frontNode->val;
-                if (frontNode->left) {
-                    q.push(frontNode->left);
-                }
-                if (frontNode->right) {
-                    q.push(frontNode->right);
-                }
+
+                int index = left ? i : (sz - 1 - i);
+                op[index] = temp->val;
+
+                if (temp->left) q.push(temp->left);
+                if (temp->right) q.push(temp->right);
             }
-            leftRight = !leftRight;
-            result.push_back(ans);
+        left=!left;
+        ans.push_back(op);
         }
-        return result;
+        return ans;
     }
 };
