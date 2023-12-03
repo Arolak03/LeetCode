706c0a9c08aa9e3081f517674882c1bea9f90855
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-12-03 23:40:31 +0530

    Time: 230 ms (5.11%), Space: 109 MB (5.46%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    int largestSubmatrix(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+    int largestSubmatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
         int ans = 0;
+        vector<pair<int, int>> prev;
         
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (matrix[row][col] != 0 && row > 0) {
-                    matrix[row][col] += matrix[row - 1][col];
+        for(int row=0;row<m;row++){
+            vector<pair<int, int>>heights;
+            vector<bool> seen=vector(n,false);
+            for(auto [height, col]: prev){
+                if(mat[row][col]==1){
+                    heights.push_back({height+1,col});
+                    seen[col]=true;
                 }
             }
-            
-            vector<int> currRow = matrix[row];
-            sort(currRow.begin(), currRow.end(), greater());
-            for (int i = 0; i < n; i++) {
-                ans = max(ans, currRow[i] * (i + 1));
+            for(int col=0;col<n;col++){
+                if(seen[col]==false && mat[row][col]==1){
+                    heights.push_back({1,col});
+                }
             }
+            for(int j=0;j<heights.size();j++){
+                ans=max(ans, (j+1)*heights[j].first);
+            }
+            prev=heights;
         }
         
         return ans;
