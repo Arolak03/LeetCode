643a78c7ff0d46b8a3e3f8413f186fd83b0090f8
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-12-22 15:38:41 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (30.36%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int maxScore(std::string s) {
-        int n = s.length();
-        int maxScore = 0;
-
-        for (int j = 1; j < n; j++) {
-            string left = s.substr(0, j);
-            string right = s.substr(j, n - j);
-
-            int leftCount = count(left.begin(), left.end(), '0');
-            int rightCount = count(right.begin(), right.end(), '1');
-
-            int currentScore = leftCount + rightCount;
-            maxScore = max(maxScore, currentScore);
+    int maxScore(string s) {
+        // string left=s.substr(s.begin(),s.length()-1);
+        // string right=s.substr(s.begin()+s.length()-1,1);
+        int lefts=0;
+        if(s[0]=='0')lefts=1;
+        int rights=0;
+        for(int i=1;i<s.length();i++)if(s[i]=='1')rights++;
+        
+        int count=0;
+        count=lefts+rights;
+        for(int i=1;i<s.length()-1;i++){
+            if(s[i]=='0')lefts++;
+            else if(s[i]=='1')rights--;
+            count=max(count, lefts+rights);
         }
-
-        return maxScore;
+        return count;
     }
 };
