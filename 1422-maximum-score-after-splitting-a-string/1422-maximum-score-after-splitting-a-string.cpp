4fc1837a08a1dfa4a1f8c763762cdcdef63e6354
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-12-22 13:31:36 +0530

    Time: 11 ms (6.25%), Space: 10.8 MB (6.25%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -5,14 +5,14 @@ public:
         int maxScore = 0;
 
         for (int j = 1; j < n; j++) {
-            std::string left = s.substr(0, j);
-            std::string right = s.substr(j, n - j);
+            string left = s.substr(0, j);
+            string right = s.substr(j, n - j);
 
-            int leftCount = std::count(left.begin(), left.end(), '0');
-            int rightCount = std::count(right.begin(), right.end(), '1');
+            int leftCount = count(left.begin(), left.end(), '0');
+            int rightCount = count(right.begin(), right.end(), '1');
 
             int currentScore = leftCount + rightCount;
-            maxScore = std::max(maxScore, currentScore);
+            maxScore = max(maxScore, currentScore);
         }
 
         return maxScore;
