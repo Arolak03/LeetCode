669c60882000129a9e958a6f766029ffb5642e27
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-12-27 21:37:05 +0530

    Time: 42 ms (22.19%), Space: 11 MB (11.59%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    #define mod 1000000007;
-    long long solve(int n, int k, int target, vector<vector<long long>>& dp){
-        if(target==0 && n==0)return 1;
-        if(target!=0 && n==0)return 0;
-        if(target==0 && n!=0)return 0;
-        if(target<0 || n<0)return 0;
-        if(dp[n][target]!=-1)return dp[n][target];
-        long long ans=0;
-        for(int i=1;i<=k;i++)ans =(ans+solve(n-1,k,target-i,dp))%mod;
-        return dp[n][target]=ans%mod;
-    }
-    
+    #define mod 1000000007
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<long long>> dp(n+1, vector<long long>(target+1,-1));
-        long long ans=solve(n,k,target,dp);
-        return ans%mod;
+        // vector<vector<long long>> dp(n+1, vector<long long>(target+1,-1));
+        vector<vector<long long>> dp(n+1, vector<long long>(target+1, 0));
+        dp[0][0]=1;
+        for(int dice=1;dice<=n;++dice){
+            for(int curr=1;curr<=target;++curr){
+                int ans = 0;
+
+                for (int num = 1; num <= k; ++num) {
+                    if (num <= curr)
+                        ans = ((ans%mod) + (dp[dice - 1][curr - num] % mod)) % mod;
+                }
+
+                dp[dice][curr] = (ans % mod);
+            
+            }
+        }
+        
+        return dp[n][target];
     }
 };
