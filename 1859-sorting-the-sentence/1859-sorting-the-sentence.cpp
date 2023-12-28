0efe8df9363ac82e502f0e9d166ed04eba379047
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-12-28 17:39:58 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (41.20%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -1,37 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <sstream>
-
-using namespace std;
-
 class Solution {
 public:
     string sortSentence(string s) {
         vector<string> mpp(10);
-        int n = s.length();
-        for (int i = n - 1; i >= 0; i--) {
-            int temp = i;
-            if ('0' <= s[i] && s[i] <= '9') {
+        int n=s.length();
+        for(int i=n-1;i>=0;i--){
+            int temp=i;
+             if ('0' <= s[i] && s[i] <= '9') {
                 while (temp >= 0 && '0' <= s[temp] && s[temp] <= '9') {
                     temp--;
                 }
-                int place = stoi(s.substr(temp + 1, i - temp));  // Corrected substring indices
-                string word = "";
-                while (temp >= 0 && s[temp] != ' ') {
-                    word += s[temp];
+                int place=stoi(s.substr(temp+1,i-temp));
+                // cout<<(s.substr(temp+1,i-temp))<<" ";
+                 string word="";
+                 
+                while(temp>=0 && s[temp]!=' '){
+                    
+                    word+=s[temp];
                     temp--;
                 }
+                
                 reverse(word.begin(), word.end());
-                mpp[place] = word;
+                 // cout<<word;
+                mpp[place]=word;
             }
         }
+        // for(auto it: mpp)cout<<it<<" ";
         string ans = "";
-        for (int i = 1; i < mpp.size(); i++) {  // Start from index 1
+        for (int i = 0; i < mpp.size(); i++) {
             ans += mpp[i];
-            if (i < mpp.size() - 1 && !mpp[i + 1].empty()) {
+            if (i < mpp.size() - 1 && !mpp[i+1].empty()) {
                 ans += ' ';
             }
         }
-        return ans;
+        string temp="";
+        for(int i=1;i<ans.length();i++){
+            temp+=ans[i];
+        }
+        return temp;
     }
 };
