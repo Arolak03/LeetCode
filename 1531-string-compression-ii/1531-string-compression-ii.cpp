64d252717b9b84b5374e14b84e84754e1c77875b
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2023-12-30 15:30:54 +0530

    Time: 725 ms (13.43%), Space: 115.2 MB (8.71%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.cpp
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -1,33 +1,27 @@
+int dp[101][101][27][101];
 class Solution {
 public:
-    int getLengthOfOptimalCompression(string s, int k) {
-        int n = s.length();
-        vector<vector<int>> dp(110, vector<int>(110, 9999)); // Initializing a 2D vector 'dp' of size 110x110 with value 9999
-        dp[0][0] = 0; // Initializing the base case where no characters and deletions exist
+   int f(int idx,int k,int prev,int prev_cnt,string& s) {
+        //base cases
+        if(k < 0) return 1e9;
+        if(idx == s.length()) return 0;
+        if(dp[idx][k][prev][prev_cnt] != -1) return dp[idx][k][prev][prev_cnt];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                int cnt = 0, del = 0;
-                for (int l = i; l >= 1; l--) {
-                    // Count the frequency of characters from 'i' to 'l'
-                    if (s[l - 1] == s[i - 1]) 
-                        cnt++;
-                    else 
-                        del++;
-                    
-                    // Check if the remaining allowed deletions are valid (j - del >= 0)
-                    if (j - del >= 0) {
-                        // Update the dp array based on the conditions
-                        dp[i][j] = min(dp[i][j],
-                            dp[l - 1][j - del] + 1 + (cnt >= 100 ? 3 : cnt >= 10 ? 2 : cnt >= 2 ? 1 : 0));
-                    }
-                }
-                
-                // If there are remaining allowed deletions (j > 0), consider the case without deleting current character
-                if (j > 0)
-                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
-            }
+        int notTake = 0 + f(idx+1,k-1,prev,prev_cnt,s);
+        int take=0;
+        if(s[idx] == (prev+'a')) {
+            take = f(idx+1,k,prev,prev_cnt+1,s);
+            take += (prev_cnt==1 || prev_cnt==9 || prev_cnt==99);
         }
-        return dp[n][k]; // Return the minimum length for 's' with at most 'k' deletions
+        else {
+            take = 1 + f(idx+1,k,s[idx]-'a',1,s);   
+        }
+        return dp[idx][k][prev][prev_cnt] = min(take,notTake);;
     }
+public:
+    int getLengthOfOptimalCompression(string s, int k) {
+        memset(dp,-1,sizeof(dp));
+        return f(0,k,26,0,s); // 26 for '{'
+    }
+    // If you understood the solution please upvote
 };
