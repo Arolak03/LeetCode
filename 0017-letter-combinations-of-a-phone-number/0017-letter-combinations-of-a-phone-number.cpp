8b2cbcdb42b5c52cb36cbc2e506688b9e1258053
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-01-04 22:22:00 +0530

    Time: 3 ms (41.96%), Space: 6.8 MB (70.01%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,33 +1,32 @@
 class Solution {
-private:
+public:
     
-    void solve(string digits, vector<string>& ans,string& temp, vector<string>& pad, int index){
-        // vector<string> output;
-        if(index==digits.size()){
-            ans.push_back(temp);
+    
+    void solve(string digit, vector<string>& test, vector<string>& ans, string& op, int index){
+        if(index==digit.length()){
+            ans.push_back(op);
             return;
         }
-        //phle process
-        string value=pad[digits[index]-'0'];
-        for(int i=0;i<value.size();i++){
-        temp.push_back(value[i]);
-        // ans.push_back(output);
-        solve(digits,ans,temp,pad,index+1);
-        temp.pop_back();
+        int ind=digit[index]-'0';
+        string ww=test[ind];
+        for(char c: ww){
+            // op+=ww[i];
+            op.push_back(c);
+            solve(digit,test,ans,op,index+1);
+            op.pop_back();
+            // solve(digit,test,ans,op,index);
         }
-
+        
+        
     }
     
-public:
     vector<string> letterCombinations(string digits) {
-        if(digits.empty()){
-            return {};
-        }
-        string temp;
-        vector<string> ans;
-    
-        vector<string> pad = {"","", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        solve(digits,ans,temp,pad, 0);
+        if(digits.empty())return {};
+        vector<string> test={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string>ans;
+        string op = "";
+        solve(digits,test,ans,op, 0);
         return ans;
+        
     }
 };
