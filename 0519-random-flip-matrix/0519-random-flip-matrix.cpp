fca4fda19e0880c11a30f0b98f31d6e8caf35715
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-01-04 22:42:14 +0530

    Time: 19 ms (65.34%), Space: 19.8 MB (34.30%) - LeetHub

--- 0519-random-flip-matrix/0519-random-flip-matrix.cpp
+++ 0519-random-flip-matrix/0519-random-flip-matrix.cpp
@@ -1,64 +1,32 @@
 class Solution {
 public:
-    int i;
-    int j;
-    int m;
-    int n;
+    set<pair<int,int>>s;
+    int r,c;
     Solution(int m, int n) {
-        // vector<vector<int>> mat(m, vector<int>(n,0));
-        this->m=m;
-        this->n=n;
-        i=0;
-        j=0;
+        c=n;
+        r=m;
     }
     
     vector<int> flip() {
-        vector<int> ans={i,j};
-        j=(j+1);
-        if(j==n){
-            j=0;
-            i+=1;
-            if(i==m){
-                i=0;
-            }
+        int x=rand()%r;
+        int y=rand()%c;
+        if(s.find({x,y})==s.end()){
+            s.insert({x,y});
+            return {x,y};
         }
-        return ans;
+        else{
+            return flip();
+        }
     }
     
     void reset() {
+        s.clear();
     }
 };
-// class Solution {
-//     int i,j;
-//     int m,n;
-// public:
-//     Solution(int m, int n) {
-//         i = 0;
-//         j = 0;
-//         this -> m = m;
-//         this -> n = n;
-//     }
-    
-//     vector<int> flip() {
-//         vector<int> ans =  {i,j};
-//         j = (j + 1);
-//         if(j == n){
-//             j = 0;
-//             i += 1;
-//             if(i == m){
-//                 i = 0;
-//             }
-//         }
-//         return ans;
-//     }
-    
-//     void reset() {
-        
-//     }
-// };
-// /**
-//  * Your Solution object will be instantiated and called as such:
-//  * Solution* obj = new Solution(m, n);
-//  * vector<int> param_1 = obj->flip();
-//  * obj->reset();
-//  */
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(m, n);
+ * vector<int> param_1 = obj->flip();
+ * obj->reset();
+ */
