a73815643d84d28fb508a73730ab2285edaf80c0
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-01-04 17:40:05 +0530

    Time: 123 ms (52.30%), Space: 85.1 MB (90.06%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+        unordered_map<int, int> mpp;
         int start=0;
         int count=0;
         int n =nums.size();
-        for(int i=0;i<nums.size();i++){
-            int y=i;
-            while(i<n && nums[start]==nums[i]){
-                i++;
+        
+        // for(int i=0;i<nums.size();i++){
+        //     int y=i;
+        //     while(i<n && nums[start]==nums[i]){
+        //         i++;
+        //     }
+        //     if(i-start==1)return -1;
+        //     if((i-start)%3==0){
+        //         count+=(i-start)/3;
+        //     }
+        //     else{
+        //         count+=(i-start)/3;
+        //         count++;
+        //     }
+        //     start=i;
+        //     i--;
+        // }
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it: mpp){
+            if(it.second==1)return -1;
+            if(it.second%3==0){
+                count+=it.second/3;
             }
-            if(i-start==1)return -1;
-            if((i-start)%3==0){
-                count+=(i-start)/3;
-            }
-            else{
-                count+=(i-start)/3;
+            else {
+                count+=it.second/3;
                 count++;
             }
-            start=i;
-            i--;
         }
         return count;
     }
