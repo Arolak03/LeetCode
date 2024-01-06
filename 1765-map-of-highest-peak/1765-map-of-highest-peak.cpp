384c6e5bdb29c1093614c9132814891c33660d67
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-01-06 14:58:47 +0530

    Time: 226 ms (98.59%), Space: 118.9 MB (34.94%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+        //bfs
         int n =mat.size();
         int m=mat[0].size();
-        queue<pair<int,int>> s;
+        vector<pair<int,int>> s;
         vector<vector<int>> ans(n, vector<int>(m,-1));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==1){
                     ans[i][j]=0;
-                    s.push({i,j});
+                    s.push_back({i,j});
                 }
             }
         }
-        // int dig=1;
+        int i=0;
         int dirx[4]={-1,1,0,0};
         int diry[4]={0,0,-1,1};
-        while(!s.empty()){
-            int x=s.front().first;
-            int y=s.front().second;
-            s.pop();
+        while(i<s.size()){
+            int x=s[i].first;
+            int y=s[i].second;
+            i++;
             for(int i=0;i<4;i++){
                 int nx=x+dirx[i];
                 int ny=y+diry[i];
                 if(nx>=0 && ny>=0 && nx<n && ny<m && ans[nx][ny]==-1){
                     ans[nx][ny]=ans[x][y]+1;
-                    s.push({nx,ny});
+                    s.push_back({nx,ny});
                 }
             }
             
