d541b17305caea406e921dca49fcbb366b374f81
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-01-09 19:04:33 +0530

    Time: 10 ms (55.54%), Space: 12.2 MB (31.91%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,12 +1,35 @@
 class Solution {
 public:
+    int findSum(vector<int>& nums, int x){
+        long long sum=0;
+        for(long long i=0;i<nums.size();i++){
+            sum+=abs(x-nums[i]);
+        }
+        return sum;
+    }
     int minMoves2(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int ans=0;
-        int n =nums.size();
-        int x=nums[nums.size()/2];
-        for(auto it: nums){
-            ans+=abs(x-it);
+       int n =nums.size();
+        int l=INT_MAX;
+       int r=INT_MIN;
+        for(int i=0;i<n;i++){
+            l=min(l,nums[i]);
+            r=max(r,nums[i]);
+        }
+        int ans=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            int val1=findSum(nums,mid);
+            int val2=findSum(nums,mid+1);
+            if(val1>val2){
+                l=mid+1;
+                ans=val2;
+            }
+            else{
+                r=mid-1;
+                ans=val1;
+            }
+            
+            
         }
         return ans;
     }
