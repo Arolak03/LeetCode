5dc0998f1d2a173c822e8272ff3c5891c623aa6c
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-01-13 14:11:27 +0530

    Time: 13 ms (44.95%), Space: 8.9 MB (62.89%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     string frequencySort(string s) {
+        // int n =s.length();
+        // unordered_map<char, int>mpp;
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     mpp[s[i]]++;
+        //     count=max(count,mpp[s[i]]);
+        // }
+        // string ans="";
+        // while(count>0 && n--){
+        //     for(auto it: mpp){
+        //         if(it.second==count){
+        //             while(it.second--)
+        //                 ans+=it.first;
+        //         }
+        //     }
+        //     count--;
+        // }
+        // return ans;
+        unordered_map<char, int> mpp;
+        priority_queue<pair<int,char>> pq;
         int n =s.length();
-        unordered_map<char, int>mpp;
-        int count=0;
         for(int i=0;i<n;i++){
             mpp[s[i]]++;
-            count=max(count,mpp[s[i]]);
         }
-        string ans="";
-        while(count>0 && n--){
-            for(auto it: mpp){
-                if(it.second==count){
-                    while(it.second--)
-                        ans+=it.first;
-                }
+        for(auto it: mpp){
+            pq.push({it.second, it.first});
+        }
+       string ans="";
+        while(!pq.empty()){
+            char temp=pq.top().second;
+            int times=pq.top().first;
+            while(times-->0){
+                ans+=temp;
             }
-            count--;
+            pq.pop();
         }
         return ans;
         
