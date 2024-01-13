14e7fecfd84373f28d46759ef9713ec275663110
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-01-13 14:00:22 +0530

    Time: 70 ms (49.19%), Space: 17.1 MB (36.03%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        unordered_map<char, int>  mpp1,mpp2;
+        unordered_map<char, int>  mpp1;
         for(int i=0;i<s.length();i++){
             mpp1[s[i]]++;
-            mpp2[t[i]]++;
+            mpp1[t[i]]--;
         }
         int count=0;
         
         
             
-        for(int i=0;i<s.length();i++){
-            if(mpp2[s[i]]!=0 && mpp2.find(s[i])!=mpp2.end()){
-                mpp2[s[i]]--;
-            }
-        }
-        for(auto it: mpp2)count+=it.second;
+        // for(int i=0;i<s.length();i++){
+        //     if(mpp2[s[i]]!=0 && mpp2.find(s[i])!=mpp2.end()){
+        //         mpp2[s[i]]--;
+        //     }
+        // }
+        for(auto it: mpp1)count+=max(it.second,0);
         return count;
     }
 };
