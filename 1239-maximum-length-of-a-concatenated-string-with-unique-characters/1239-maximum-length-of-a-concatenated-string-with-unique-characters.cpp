4e38a7a849dcc5d84224c3abd0235f0a6f373d42
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-01-23 17:12:45 +0530

    Time: 136 ms (38.57%), Space: 98.9 MB (23.48%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,22 +1,49 @@
 class Solution {
 public:
-    int maxLength(vector<string>& A) {
-        vector<bitset<26>> dp = {bitset<26>()};
-        int res = 0;
-        for (auto& s : A) {
-            bitset<26> a;
-            for (char c : s)
-                a.set(c - 'a');
-            int n = a.count();
-            if (n < s.size()) continue;
+    int ans=INT_MIN;
+    bool unique(string s){
+        return unordered_set<char> (s.begin(), s.end()).size()==s.length();
+    }
+    
+    int solve(string s, vector<string>& arr, int index){
+        if(index>=arr.size()){
+            ans=max((int)s.length(),ans);
+            return ans;
+        }
+        int val1=solve(s,arr,index+1);
 
-            for (int i = dp.size() - 1; i >= 0; --i) {
-                bitset c = dp[i];
-                if ((c & a).any()) continue;
-                dp.push_back(c | a);
-                res = max(res, (int)c.count() + n);
+        bool flag=1;
+        //hr word k chack krnna h 
+        for(auto c: arr[index]){
+            if(s.find(c)!=string::npos || !unique(arr[index])){
+                flag=0;
+                break;
             }
         }
-        return res;
+        int val2=INT_MIN;
+        if(flag){
+            val2=solve(s+arr[index],arr,index+1);
+        }
+        return max(val1,val2);
+    }
+    
+    
+    int maxLength(vector<string>& arr) {
+        // int ans=0;
+        solve("",arr,0);
+        return ans;
     }
 };
+        
+//         int val2 = INT_MIN;
+//         if(flag) 
+//         {
+//             val2 = f(i+1,arr,s+arr[i]);
+//         }
+//         return max(val1,val2);
+//     }
+//     int maxLength(vector<string>& arr) {
+//         f(0,arr,"");
+//         return ans;
+//     }
+// };
