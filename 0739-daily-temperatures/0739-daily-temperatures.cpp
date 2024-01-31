2c3faef7ee87dac070c65dc93f35af874e90e9c3
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-01-31 09:30:05 +0530

    Time: 311 ms (8.93%), Space: 100.9 MB (68.65%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        stack<int> s;
+        int n = t.size();
+        vector<int> right(n,0);
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && t[s.top()]<=t[i]){
+                s.pop();
+            }
+            if(!s.empty())cout<<s.top()<<" ";
+            if(!s.empty())right[i]=s.top();
+            s.push(i);
+        }
+        for(int i=0;i<n;i++){
+            if(right[i]>0)right[i]=right[i]-i;
+        }
+        return right;
+        
+    }
+};
