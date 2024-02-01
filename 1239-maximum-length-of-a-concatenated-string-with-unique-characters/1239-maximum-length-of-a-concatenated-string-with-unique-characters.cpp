2aee178afac284332cea173709ee35d6deede38f
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-02-01 14:47:55 +0530

    Time: 105 ms (37.01%), Space: 17.5 MB (58.88%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,49 +1,53 @@
 class Solution {
 public:
-    int ans=INT_MIN;
+
     bool unique(string s){
         return unordered_set<char> (s.begin(), s.end()).size()==s.length();
     }
-    
-    int solve(string s, vector<string>& arr, int index){
-        if(index>=arr.size()){
-            ans=max((int)s.length(),ans);
-            return ans;
-        }
-        int val1=solve(s,arr,index+1);
-
-        bool flag=1;
-        //hr word k chack krnna h 
-        for(auto c: arr[index]){
-            if(s.find(c)!=string::npos || !unique(arr[index])){
-                flag=0;
-                break;
+    bool dupli(string s,string arr){
+        int arr1[26]={0};
+        for(char &ch: s){
+            if(arr1[ch-'a']>0)return false;
+            else{
+                arr1[ch-'a']++;
+                // cout<<ch<<" ";
             }
         }
-        int val2=INT_MIN;
-        if(flag){
-            val2=solve(s+arr[index],arr,index+1);
+        for(int i=0;i<arr.length();i++){
+            if(arr1[arr[i]-'a']!=0)return false;
         }
-        return max(val1,val2);
+        // cout<<endl;
+        return true;
+    }
+    
+    int solve(string s, vector<string>& arr, int index, int& ans){
+        if(index>=arr.size()){
+            ans=max(ans,(int)s.length());
+            return s.length();
+        }
+
+        int include=0;
+        int exc=0;
+        if(!dupli(arr[index], s)){
+            exc=solve(s,arr,index+1,ans);
+            // cout<<"yes"<<" ";
+        }
+        else{
+            exc=solve(s,arr,index+1,ans);
+            include=solve(s+arr[index],arr,index+1,ans);
+       }
+        cout<<include<<" "<<exc<<s.length()<<endl;
+        return max(include,exc);   
     }
     
     
     int maxLength(vector<string>& arr) {
         // int ans=0;
-        solve("",arr,0);
+        // int ans=0;
+        string temp="";
+        int ans=0;
+        solve(temp,arr,0,ans);
         return ans;
     }
 };
         
-//         int val2 = INT_MIN;
-//         if(flag) 
-//         {
-//             val2 = f(i+1,arr,s+arr[i]);
-//         }
-//         return max(val1,val2);
-//     }
-//     int maxLength(vector<string>& arr) {
-//         f(0,arr,"");
-//         return ans;
-//     }
-// };
