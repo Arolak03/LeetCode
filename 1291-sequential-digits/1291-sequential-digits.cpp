affd55451ed4d40066997f321408f9469ead07d9
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-02-02 13:46:31 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (13.81%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,85 +1,21 @@
 class Solution {
+    int arr[36] = {
+        12, 23, 34, 45, 56, 67, 78, 89,
+        123, 234, 345, 456, 567, 678, 789,
+        1234, 2345, 3456, 4567, 5678, 6789,
+        12345, 23456, 34567, 45678, 56789,
+        123456, 234567, 345678, 456789,
+        1234567, 2345678, 3456789,
+        12345678, 23456789,
+        123456789};
 public:
     vector<int> sequentialDigits(int low, int high) {
-        // int dig = 0;
-        // int x=low;
-        // int fact=0;
-        // while(x){
-        //     x/=10;
-        //     dig++;
-        //     fact+=1;
-        //     fact*=10;
-        // }
-        // //first
-        // x=low;
-        // int leftfMost=0;
-        // while(x>=10){
-        //     x/=10;
-        // }
-        // leftMost=x;
-        // int temp=dig-1;
-        // int neww=leftMost;
-        // while(temp--){
-        //     new*=10;
-        //     new+=leftMost+1;
-        // }
-        // vector<int> ans;
-        // for(int i=low;i<=high;i++){
-        //     int temp=i;
-        //     int dig=temp%10;
-        //     temp/=10;
-        //     bool flag=true;
-        //     while(temp){
-        //         int dig2=temp%10;
-        //         if(dig2+1==dig){
-        //             temp/=10;
-        //             dig=dig2;
-        //         }
-        //         else{
-        //             flag=false;
-        //             break;
-        //         }
-        //     }
-        //     if(flag){
-        //         ans.push_back(i);
-        //     }
-        // }
-        // return ans;
-        
-        // int dig=0;
-        // int x=low;
-        // while(x){
-        //     dig++;
-        //     x/=10;
-        // }
-        // int fact=0;
-        // int num=0;
-        // int tempDig=dig;
-        // while(tempDig--){
-        //     fact+=1;
-        //     fact*=10;
-        // }
-        // fact/=100;
-        // vector<int> ans;
-        queue<int> q;
-        for(int i=1;i<9;i++){
-            q.push(i);
-        }
         vector<int> ans;
-        while(!q.empty()){
-            int top=q.front();
-            q.pop();
-            if(top>=low && top<=high)ans.push_back(top);
-            if(top>high)break;
-            // int rem=top%10;
-            if(top%10<9){
-                int rem=top%10;
-                top=top*10+rem+1;
-                q.push(top);
-            }
+        for(int i=0;i<36;i++){
+            if(arr[i]<low)continue;
+            if(arr[i]>high)break;
+            ans.push_back(arr[i]);
         }
         return ans;
-        
-        
     }
 };
