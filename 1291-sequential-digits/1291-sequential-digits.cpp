a3ff85953afcd59fc6f3d488ebfdce3f10f7d539
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-02-02 13:47:53 +0530

    Time: 3 ms (17.15%), Space: 7.7 MB (8.37%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,21 +1,17 @@
 class Solution {
-    int arr[36] = {
-        12, 23, 34, 45, 56, 67, 78, 89,
-        123, 234, 345, 456, 567, 678, 789,
-        1234, 2345, 3456, 4567, 5678, 6789,
-        12345, 23456, 34567, 45678, 56789,
-        123456, 234567, 345678, 456789,
-        1234567, 2345678, 3456789,
-        12345678, 23456789,
-        123456789};
 public:
+    vector<int> ans;
+    void dfs(int low,int high,int i,int num){
+        if(num >= low && num <= high)
+           ans.push_back(num);
+        if(num > high || i>9 )
+          return;
+        dfs(low,high,i+1,num*10+i);
+    }
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> ans;
-        for(int i=0;i<36;i++){
-            if(arr[i]<low)continue;
-            if(arr[i]>high)break;
-            ans.push_back(arr[i]);
-        }
+        for(int i=1;i<=9;i++)
+            dfs(low,high,i,0);
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
