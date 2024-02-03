5652ac6016540ec0dfac211466c4efa40a24ef63
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-02-03 20:45:32 +0530

    Time: 11 ms (74.73%), Space: 11 MB (8.52%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    int maxSum(vector<int>& arr, int k, int dp[], int start) {
-        int N = arr.size();
-        
-        if (start >= N) {
-            return 0;
+    int solve(vector<int>& arr, int start, int k, vector<int>& dp){
+        if(start>=arr.size())return 0;
+        if(dp[start]!=-1)return dp[start];
+        int n =arr.size();
+        int end=min(n,start+k);
+        int ans=0;
+        int currMax=0;
+        for(int i=start;i<end;i++){
+            currMax=max(currMax,arr[i]);
+            ans=max(ans, currMax*(i-start+1)+solve(arr,i+1,k,dp));
         }
-        
-        // Return the already calculated answer.
-        if (dp[start] != -1) {
-            return dp[start];
-        }
-        
-        int currMax = 0, ans = 0;
-        int end = min(N, start + k);
-        for (int i = start; i < end; i++) {
-            currMax = max(currMax, arr[i]);
-            // Store the maximum of all options for the current subarray.
-            ans = max(ans, currMax * (i - start + 1) + maxSum(arr, k, dp, i + 1));
-        }
-
-        // Store the answer to be reused.
-        return dp[start] = ans;
+        return dp[start]=ans;
     }
     
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        int dp[arr.size()];
-        memset(dp, -1, sizeof(dp));
-        
-        return maxSum(arr, k, dp, 0);
+        vector<int> dp(arr.size()+1,-1);
+        return solve(arr,0,k,dp);
     }
 };
