a7f2b3597957cdd19c83c4d0b1423678b70d7dbe
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-02-04 23:33:03 +0530

    Time: 330 ms (92.04%), Space: 168.4 MB (68.76%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,36 +1,88 @@
 class LRUCache {
 public:
-    unordered_map<int, list<pair<int, int>>::iterator> mpp;
-    list<pair<int, int>> lrulist;  // key-value pairs
-    int size;
+    
+    struct ListNode{
+        ListNode* prev;
+        ListNode* next;
+        int data;
+        int key;
+        ListNode(int key, int data) {
+            this->key = key;
+            this->data = data;
+            prev = NULL;
+            next = NULL;
+        }
 
+    };
+    ListNode* head;
+    ListNode*tail;
+    unordered_map<int,ListNode*> mpp;
+    int k;
+    void moveToHead(ListNode* node){
+        if(node==head)return;
+        if(node==tail){
+            tail=tail->prev;
+            tail->next=NULL;
+        }
+        else{
+            node->prev->next=node->next;
+            node->next->prev=node->prev;
+        }
+        node->next=head;
+        head->prev=node;
+        head=node;
+        node->prev=NULL;
+    }
     LRUCache(int capacity) {
-        size = capacity;
+        k=capacity;
+        head=NULL;
+        tail=NULL;
     }
-
+    
+    
     int get(int key) {
-        if (mpp.find(key) != mpp.end()) {
-            auto lrulistIterator = mpp[key];
-            lrulist.splice(lrulist.begin(), lrulist, lrulistIterator);
-            return lrulistIterator->second;
-        } else {
-            return -1;
+        if(mpp.find(key)!=mpp.end()){
+            ListNode* curr=mpp[key];
+            moveToHead(curr);
+            return curr->data;
         }
+       else{
+           return -1;
+       }
     }
-
+    
     void put(int key, int value) {
-        if (mpp.find(key) != mpp.end()) {
-            auto lrulistIterator = mpp[key];
-            lrulistIterator->second = value;  // Update the value
-            lrulist.splice(lrulist.begin(), lrulist, lrulistIterator);
-        } else {
-            if (lrulist.size() >= size) {
-                int toRemove = lrulist.back().first;
-                lrulist.pop_back();
-                mpp.erase(toRemove);
+    if (mpp.find(key) != mpp.end()) {
+        ListNode* curr = mpp[key];
+        curr->data = value;
+        moveToHead(curr);
+    } else {
+        ListNode* temp = new ListNode(key, value);
+        if (mpp.size() >= k) {
+            mpp.erase(tail->key);
+            if (tail->prev) {
+                tail = tail->prev;
+                tail->next = NULL;
+            } else {
+                head = tail = NULL;
             }
-            lrulist.push_front({key, value});
-            mpp[key] = lrulist.begin();
+        }
+        mpp[key] = temp;
+        if (!head) {
+            head = tail = temp;
+        } else {
+            temp->next = head;
+            head->prev = temp;
+            head = temp;
         }
     }
+}
+
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
