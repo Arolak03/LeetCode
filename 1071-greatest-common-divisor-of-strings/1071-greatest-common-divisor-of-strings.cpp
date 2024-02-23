b9e565df7435d6d3861fff0ff7d147f790dd08ab
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-02-23 23:51:02 +0530

    Time: 6 ms (26.74%), Space: 8.6 MB (22.15%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -22,6 +22,19 @@ public:
     //     return ans;
     // }
     
+    
+    
+    void gcdd(int n1, int n2, int index, int& prod){
+        if(index>n1 || index>n2)return;
+        if(n1%index==0 && n2%index==0){
+            prod*=index;
+            gcdd(n1/index,n2/index,index,prod);
+        }
+        else{
+            gcdd(n1,n2,index+1,prod);
+        }
+    }
+    
     string gcdOfStrings(string str1, string str2) {
         // int n1=str1.length();
         // int n2=str2.length();
@@ -33,7 +46,9 @@ public:
         // }
         // return "";
         if(str1+str2 != str2+str1)return "";
-        int gcdLen=gcd(str1.size(), str2.size());
-        return str1.substr(0,gcdLen);
+        // int gcdLen=gcd(str1.size(), str2.size());
+        int prod=1;
+        gcdd(str1.length(), str2.length(),2,prod);
+        return str1.substr(0,prod);
     }
 };
