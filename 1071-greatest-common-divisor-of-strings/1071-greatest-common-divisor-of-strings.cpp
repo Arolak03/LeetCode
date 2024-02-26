111848fb58286f37add50f5b574905a905d05064
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-02-26 11:53:40 +0530

    Time: 6 ms (27.01%), Space: 8.6 MB (40.00%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -2,26 +2,7 @@ class Solution {
 public:
     
     
-    // bool validLen(string s1, string s2, int len){
-    //     int len1=s1.length();
-    //     int len2=s2.length();
-    //     if(s1.length()%len!=0 || s2.length()%len!=0)return false;
-    //     else{
-    //         string base=s1.substr(0,len);
-    //         int n1=len1/len;
-    //         int n2=len2/len;
-    //         return s1==join(base,n1) && s2==join(base,n2);
-    //     }
-    // }
-    // string join(string s, int len){
-    //     string ans="";
-    //     //n1 times mai base add krungi 
-    //     for(int i=0;i<len;i++){
-    //         ans+=s;
-    //     }
-    //     return ans;
-    // }
-    
+  
     
     
     void gcdd(int n1, int n2, int index, int& prod){
@@ -36,17 +17,8 @@ public:
     }
     
     string gcdOfStrings(string str1, string str2) {
-        // int n1=str1.length();
-        // int n2=str2.length();
-        // if(str1+str2 != str2+str1)return "";
-        // for(int i=min(n2,n1);i>=1;i--){
-        //     if(validLen(str1,str2,i)){
-        //         return str1.substr(0,i);
-        //     }
-        // }
-        // return "";
+       
         if(str1+str2 != str2+str1)return "";
-        // int gcdLen=gcd(str1.size(), str2.size());
         int prod=1;
         gcdd(str1.length(), str2.length(),2,prod);
         return str1.substr(0,prod);
