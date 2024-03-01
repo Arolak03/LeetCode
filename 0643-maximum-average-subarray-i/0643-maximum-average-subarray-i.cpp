a9c1102154e080308e4659ea2e074850dfe46ca5
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-01 21:59:47 +0530

    Time: 71 ms (99.05%), Space: 112.4 MB (30.26%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -5,6 +5,9 @@ public:
         int n = nums.size();
         int i=0;
         int j=i;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         double avg=0;
         int m=k;
         
