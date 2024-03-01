2064a0d660c4bf4057860753e8babe7eb1b18022
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-01 22:07:31 +0530

    Time: 139 ms (31.35%), Space: 112.5 MB (30.26%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -5,23 +5,27 @@ public:
         int n = nums.size();
         int i=0;
         int j=i;
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
         double avg=0;
+        int start=0;
         int m=k;
-        
         while(m--){
-            avg+=nums[j];
-            j++;
+            avg+=nums[i++];
+            // j++;
         }
         ans=max(ans,avg/k);
-        while(i<n-k && j<n){
-           avg=avg-nums[i]+nums[j];
-           i++;
-           j++;
-           ans=max(ans,avg/k);
+        
+        // while(i<n-k && j<n){
+        //    avg=avg-nums[i]+nums[j];
+        //    i++;
+        //    j++;
+        //    ans=max(ans,avg/k);
+        // }
+        for(int r=k;r<n;r++){
+            avg-=nums[start++];
+            avg+=nums[r];
+            ans=max(ans,avg/k);
         }
+        
         return ans;
     }
 };
