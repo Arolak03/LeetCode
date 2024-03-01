64938cdc9787ed282ba30a81b1744096f6327266
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-01 21:56:38 +0530

    Time: 252 ms (6.39%), Space: 112.3 MB (52.66%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double ans=INT_MIN;
+        int n =nums.size();
+        // for(int i=0;i<=n-k;i++){
+        //     double avg=0;
+        //     for(int j=i;j<i+k;j++){
+        //         avg+=nums[j];
+        //     }
+        //     ans=max(ans,avg/k);
+        // }
+        
+        int i=0;
+        int j=i;
+        double avg=0;
+        int m=k;
+        
+        while(m--){
+            avg+=nums[j];
+            cout<<nums[j]<<" ";
+            j++;
+        }
+        ans=max(ans,avg/k);
+        while(i<n-k && j<n){
+           avg-=nums[i];
+            i++;
+            // j++;
+           avg+=nums[j];
+           j++;
+            ans=max(ans,avg/k);
+        }
+        return ans;
+    }
+};
