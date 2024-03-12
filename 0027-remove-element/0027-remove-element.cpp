edfbd15851b3f90e79007961ebb9c8d35c3457e9
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-12 17:14:19 +0530

    Time: 0 ms (100.00%), Space: 10.5 MB (33.32%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,7 +3,7 @@ public:
     int removeElement(vector<int>& nums, int val) {
         // int n =nums.size();
         int count=0;
-        // vector<int> ans;
+    
         
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=val){
@@ -11,9 +11,9 @@ public:
                 nums[count]=nums[i];
                 count++;
             }
-            // else ans.push_back(nums[i]);
+            
         }
-        // nums=ans;
-        return count;;
+
+        return count;
     }
 };
