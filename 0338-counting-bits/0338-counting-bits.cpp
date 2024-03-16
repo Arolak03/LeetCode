20adedb0aacd588ef3dd091d8ceb328e28ad1aaa
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-16 23:57:25 +0530

    Time: 3 ms (72.27%), Space: 8.9 MB (76.24%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    
-    int solve(int x){
-        int count=0;
-        while(x){
-            if((x & 1) == 1)count++;
-            x>>=1;
-        }
-        return count;
+  vector<int> countBits(int n) {
+    vector<int> dp(n + 1, 0); // dp array to store results
+    if(n==0)return {0};
+    // Base cases (0 and 1 have 0 and 1 set bits, respectively)
+    dp[0] = 0;
+    dp[1] = 1;
+
+    // Build up the dp table based on a pattern
+    for (int i = 2; i <= n; ++i) {
+      // Even numbers have the same number of set bits as their previous even number divided by 2
+      if (i % 2 == 0) {
+        dp[i] = dp[i / 2];
+      } else {
+        // Odd numbers have 1 more set bit than their previous odd number
+        dp[i] = dp[i - 1] + 1;
+      }
     }
-    
-    
-    vector<int> countBits(int n) {
-        vector<int> ans;
-        for(int i=0;i<=n;i++){
-            // int x=solve(i);
-            ans.push_back(solve(i));
-        }
-        return ans;
-    }
+
+    return dp;
+  }
 };
