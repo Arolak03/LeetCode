8a7612a5cda57870d66a3bc4ecc63fbc14c47e43
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-16 15:39:10 +0530

    Time: 215 ms (41.64%), Space: 85.3 MB (88.54%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -3,19 +3,36 @@ public:
     int distributeCandies(vector<int>& c) {
         int n =c.size();
         int temp=n/2;
-        unordered_map<int, int> mpp;
-        for(int i=0;i<n && temp>0;i++){
-            if(mpp.count(c[i])){
-                
-                continue;
+//         unordered_map<int, int> mpp;
+//         for(int i=0;i<n && temp>0;i++){
+//             if(mpp.count(c[i])){
+    
+//                 continue;
+//             }
+//             else{
+            
+//                 mpp[c[i]]++;
+//                 temp--;
+//             }
+//         }
+    
+//         return mpp.size();
+        int i=0;
+        int ans=0;
+        
+        sort(c.begin(), c.end());
+        while(i<n && temp>0){
+            if(i>0 && c[i]==c[i-1]){
+                i++;
+                // cout<<c[i]<<"*";
             }
             else{
-            
-                mpp[c[i]]++;
+                ans++;
+                cout<<c[i]<<" ";
+                i++;
                 temp--;
             }
         }
-    
-        return mpp.size();
+        return ans;
     }
 };
