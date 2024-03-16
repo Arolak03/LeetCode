8579faca44eed95dd4fc6e681abd351ace00aae6
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-16 23:40:42 +0530

    Time: 138 ms (11.60%), Space: 60.6 MB (92.18%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -2,18 +2,18 @@ class RecentCounter {
 public:
     priority_queue<int, vector<int> , greater<int>> pq;
     RecentCounter() {
-        // priority_queue<int> pq;
+        
         
     }
     
     int ping(int t) {
-        // cout<<pq.top()<<" ";
+        
         while(!pq.empty() && pq.top()<t-3000){
             cout<<pq.top();
             pq.pop();
         }
         pq.push(t);
-        cout<<pq.top()<<" ";
+        
         return pq.size();
     }
 };
