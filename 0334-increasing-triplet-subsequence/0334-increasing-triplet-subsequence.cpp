946ca0f56d97a8ab60280ccc7dacc51574f979db
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-16 23:18:31 +0530

    Time: 135 ms (5.26%), Space: 123.9 MB (5.47%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
- bool increasingTriplet(vector<int>& nums) {
-
-
-        
-        int firstSmaller = INT_MAX, secondSmaller = INT_MAX;
-
-        for(int i=0 ;i<nums.size() ;i++){
-
-            if(firstSmaller>=nums[i])
-            firstSmaller = nums[i];
-            else if(secondSmaller>=nums[i])
-            secondSmaller = nums[i];
-            else
-            return 1;
+    
+    void nxt(vector<int>& nums, vector<int>& next){
+        int n = nums.size();
+        stack<int> s;
+        s.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(!s.empty() && nums[s.top()]<=nums[i])s.pop();
+            if(!s.empty())next[i]=s.top();
+            s.push(i);
         }
-        return 0;
+    }
+    void prevv(vector<int>& nums, vector<int>& prev){
+        int n =nums.size();
+        stack<int> s;
+        s.push(0);
+        for(int i=1;i<n;i++){
+            while(!s.empty() && nums[s.top()]>=nums[i])s.pop();
+            if(!s.empty())prev[i]=s.top();
+            s.push(i);
+        }
+    }
+    
+    
+    bool increasingTriplet(vector<int>& nums) {
+        int n =nums.size();
+        vector<int> prev(n,-1);
+        vector<int> next(n,-1);
+        nxt(nums,next);
+        prevv(nums,prev);
+        for(int i=0;i<n;i++){
+            if(prev[i]!=-1 && next[i]!=-1)return true;
+        }
+        return false;
     }
 };
