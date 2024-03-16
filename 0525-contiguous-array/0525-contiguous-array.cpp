9e79fa54371784a98d1d9f121bfae09c68b29e92
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-16 10:33:10 +0530

    Time: 84 ms (66.28%), Space: 87.8 MB (53.36%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-  int findMaxLength(vector<int>& nums) {
-    int maxLength = 0;
-    int sum = 0;  // Keep track of the prefix sum (number of 1s minus number of 0s)
-    unordered_map<int, int> map; // Map sum to the first index seen with that sum
-
-    map[0] = -1; // Initialize with sum 0 and index -1 (no previous occurrence)
-
-    for (int i = 0; i < nums.size(); ++i) {
-      sum += nums[i] == 1 ? 1 : -1;  // Add 1 for 1s, subtract 1 for 0s
-
-      // Check if the current sum has been seen before
-      if (map.count(sum)) {
-        maxLength = max(maxLength, i - map[sum]);  // Update if current subarray is longer
-      } else {
-        map[sum] = i;  // Add the current sum and its index to the map
-      }
+    int findMaxLength(vector<int>& nums) {
+        int n =nums.size();
+        int sum=0;
+        int maxLen=0;
+        unordered_map<int, int> mpp;
+        mpp[0]=-1;
+        for(int i=0;i<n;i++){
+            sum+= nums[i]==1? 1: -1;
+            if(mpp.count(sum)){
+                maxLen=max(maxLen,i-mpp[sum]);
+            }
+            else {
+                mpp[sum]=i;
+            }
+        }
+        return maxLen;
     }
-
-    return maxLength;
-  }
 };
