85a5fb6e9f1eb2c272782b186bd692295c09b37a
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-16 23:52:25 +0530

    Time: 9 ms (11.14%), Space: 9.6 MB (25.37%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -5,7 +5,6 @@ public:
         int count=0;
         while(x){
             if((x & 1) == 1)count++;
-            // cout<<x<<" ";;
             x>>=1;
         }
         return count;
@@ -15,8 +14,8 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i=0;i<=n;i++){
-            int x=solve(i);
-            ans.push_back(x);
+            // int x=solve(i);
+            ans.push_back(solve(i));
         }
         return ans;
     }
