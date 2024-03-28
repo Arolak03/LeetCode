38c68953cb7beaef8f219c8850a2624ac822ec98
Arolak03 <100058791+Arolak03@users.noreply.github.com>
2024-03-28 14:53:42 +0530

    Time: 7 ms (71.15%), Space: 13.1 MB (93.70%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -1,26 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
-        ListNode* front = new ListNode(0, head);
-        ListNode* start = front;
-
-        while (start != nullptr) {
-            int prefixSum = 0;
-            ListNode* end = start->next;
-
-            while (end != nullptr) {
-                // Add end's value to the prefix sum
-                prefixSum += end->val;
-                // Delete zero sum consecutive sequence 
-                // by setting node before sequence to node after
-                if (prefixSum == 0) {
-                    start->next = end->next;
+        // ListNode* end=head;
+        ListNode* start=new ListNode(0,head);
+        ListNode* temp=start;
+        // start->next=head;
+        // ListNode* temp=start;
+        // ListNode* ans=temp;
+        // start=start->next;
+        while(start!=NULL){
+            int sum=0;
+            ListNode* end=start->next;
+            while(end!=NULL){
+                sum+=end->val;
+                if(sum==0){
+                    // temp->next=end->next;
+                    start->next=end->next;
+                    // temp=temp->next;
                 }
-                end = end->next;
+                end=end->next;
             }
-
-            start = start->next;
+            start=start->next;
         }
-        return front->next;
+        return temp->next;
     }
 };
